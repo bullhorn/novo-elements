15e5fe0ebdc10476aac1b93560b0b4cf7357a8ad
Joshua Godi <jgodi@bullhorn.com>
2016-04-28 14:17:34 -0500

    feat(deferred): Implement deferred object

--- src/novo-elements.js
+++ src/novo-elements.js
@@ -3,10 +3,19 @@ import { NOVO_TAB_ELEMENTS } from './elements/tabs';
 import { NOVO_CARD_ELEMENTS, NOVO_CARD_EXTRA_ELEMENTS } from './elements/card';
 import { NOVO_LOADING_ELEMENTS } from './elements/loading';
 
+// Elements
 export * from './elements/button';
 export * from './elements/tabs';
 export * from './elements/card';
 export * from './elements/loading';
+
+// Pipes
+export * from './pipes/plural/Plural';
+
+// Utils
+export * from './utils/outside-click/OutsideClick';
+export * from './utils/key-codes/KeyCodes';
+export * from './utils/deferred/Deferred';
 
 export const NOVO_ELEMENTS = [
     NOVO_BUTTON_ELEMENTS,
