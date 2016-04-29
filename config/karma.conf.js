8d4ce304b63440786ef0a7a02276adffcc91d8cd
Joshua Godi <jgodi@bullhorn.com>
2016-04-28 20:54:10 -0500

    chore(coverage): Making code coverage reflect actual source code, closes #25

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: { './config/spec-bundle.js': ['coverage', 'webpack', 'sourcemap'] },
+        preprocessors: { './config/spec-bundle.js': ['webpack'] },
 
         // Webpack Config at ./webpack.test.js
         webpack: testWebpackConfig,
