91d13bf91e1ea19677f2c17e39c8d440d599a620
Joshua Godi <jgodi@bullhorn.com>
2016-05-01 20:13:22 -0500

    Adding reflect to the spec bundle and setting the model on the test component

--- src/elements/select/Select.spec.js
+++ src/elements/select/Select.spec.js
@@ -9,12 +9,13 @@ import { Select } from './Select';
 @Component({
     selector: 'test-cmp',
     directives: [Select],
-    template: '<novo-select [options]="options" [placeholder]="placeholder"></novo-select>'
+    template: '<novo-select [options]="options" [placeholder]="placeholder" [(ngModel)]="value" disabled></novo-select>'
 })
 class TestCmp {
     constructor() {
         this.placeholder = 'Testing';
         this.options = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+        this.value = 'Alaska';
     }
 }
 
