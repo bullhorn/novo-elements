88050d7a5e48d1ee2f1b16fc43008bb3453ee5ce
Alec Sibilia <asibilia@bullhorn.com>
2016-05-09 14:37:23 -0400

    updated calendar documentation

--- demo/pages/calendar/CalendarDemo.js
+++ demo/pages/calendar/CalendarDemo.js
@@ -10,19 +10,19 @@ import TimeDemoTpl from './templates/TimeDemo.html';
 const template = `
 <div class="container">
     <h1>Date and Time Pickers</h1>
-    <p>Calendar and Time pickers give users a more unique visual experience than standard input fields.</p>
+    <p>These allow users to easily select a time and date. It comes in a handful of varieties based on the content of the field.</p>
 
-    <h2>Calendar Picker</h2>
-    <p>Calendar pickers are used to pick dates and can be used inline in a form or as a standalone modal.</p>
+    <h2>Calendar Picker  <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/tree/master/src/elements/date-picker">(source)</a></small></h2>
+    <p>The calendar picker is used to select a date. It appears in all date picker fields.</p>
 
-    <h5>Full Calendar Picker <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/tree/master/src/elements/date-picker">(source)</a></small></h5>
+    <h5>Full Calendar Picker</h5>
     <div class="example demo">${CalendarDemoTpl}</div>
     <code-snippet [code]="CalendarDemoTpl"></code-snippet>
 
-    <h2>Time Picker</h2>
+    <h2>Time Picker  <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/tree/master/src/elements/time-picker">(source)</a></small></h2>
     <p>Time pickers come in 12 hour or 24 hour style.</p>
 
-    <h5>Standalone Time Picker <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/tree/master/src/elements/time-picker">(source)</a></small></h5>
+    <h5>Standalone Time Picker</h5>
     <div class="example demo">${TimeDemoTpl}</div>
     <code-snippet [code]="TimeDemoTpl"></code-snippet>
 </div>
