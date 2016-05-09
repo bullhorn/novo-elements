9e150eac3443f72a761955f1c1344d35ad9d36dd
Alec Sibilia <asibilia@bullhorn.com>
2016-05-09 12:46:53 -0400

    updated calendar documentation

--- demo/pages/calendar/CalendarDemo.js
+++ demo/pages/calendar/CalendarDemo.js
@@ -10,24 +10,19 @@ import TimeDemoTpl from './templates/TimeDemo.html';
 const template = `
 <div class="container">
     <h1>Date and Time Pickers</h1>
-    <p></p>
+    <p>Calendar and Time pickers give users a more unique visual experience than standard input fields.</p>
 
     <h2>Calendar Picker</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non massa et augue molestie lacinia. Ut purus sem, bibendum at blandit vitae, bibendum sed turpis. Fusce eros libero, fringilla non dapibus in, cursus elementum ipsum.</p>
-
-    <h5>Full Calendar Picker</h5>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non massa et augue molestie lacinia. Ut purus sem, bibendum at blandit vitae, bibendum sed turpis. Fusce eros libero, fringilla non dapibus in, cursus elementum ipsum.</p>
+    <p>Calendar pickers are used to pick dates and can be used inline in a form or as a standalone modal.</p>
 
     <h5>Full Calendar Picker <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/tree/master/src/elements/date-picker">(source)</a></small></h5>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non massa et augue molestie lacinia. Ut purus sem, bibendum at blandit vitae, bibendum sed turpis. Fusce eros libero, fringilla non dapibus in, cursus elementum ipsum.</p>
     <div class="example demo">${CalendarDemoTpl}</div>
     <code-snippet [code]="CalendarDemoTpl"></code-snippet>
 
     <h2>Time Picker</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non massa et augue molestie lacinia. Ut purus sem, bibendum at blandit vitae, bibendum sed turpis. Fusce eros libero, fringilla non dapibus in, cursus elementum ipsum.</p>
+    <p>Time pickers come in 12 hour or 24 hour style.</p>
 
     <h5>Standalone Time Picker <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/tree/master/src/elements/time-picker">(source)</a></small></h5>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non massa et augue molestie lacinia. Ut purus sem, bibendum at blandit vitae, bibendum sed turpis. Fusce eros libero, fringilla non dapibus in, cursus elementum ipsum.</p>
     <div class="example demo">${TimeDemoTpl}</div>
     <code-snippet [code]="TimeDemoTpl"></code-snippet>
 </div>
