2dff88e00173e645df00e3387b36c50b2111f5ea
Alec Sibilia <alec.sibilia@bluetrainmobile.com>
2016-05-10 19:56:28 -0400

    chore(cleanup): Cleaning up styling/demos

--- demo/pages/calendar/CalendarDemo.js
+++ demo/pages/calendar/CalendarDemo.js
@@ -10,24 +10,19 @@ import TimeDemoTpl from './templates/TimeDemo.html';
 const template = `
 <div class="container">
     <h1>Date and Time Pickers</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non massa et augue molestie lacinia. Ut purus sem, bibendum at blandit vitae, bibendum sed turpis. Fusce eros libero, fringilla non dapibus in, cursus elementum ipsum.</p>
+    <p>These allow users to easily select a time and date. It comes in a handful of varieties based on the content of the field.</p>
 
-    <h2>Calendar Picker</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non massa et augue molestie lacinia. Ut purus sem, bibendum at blandit vitae, bibendum sed turpis. Fusce eros libero, fringilla non dapibus in, cursus elementum ipsum.</p>
+    <h2>Calendar Picker  <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/tree/master/src/elements/date-picker">(source)</a></small></h2>
+    <p>The calendar picker is used to select a date. It appears in all date picker fields.</p>
 
     <h5>Full Calendar Picker</h5>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non massa et augue molestie lacinia. Ut purus sem, bibendum at blandit vitae, bibendum sed turpis. Fusce eros libero, fringilla non dapibus in, cursus elementum ipsum.</p>
-
-    <h5>Full Calendar Picker <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/tree/master/src/elements/date-picker">(source)</a></small></h5>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non massa et augue molestie lacinia. Ut purus sem, bibendum at blandit vitae, bibendum sed turpis. Fusce eros libero, fringilla non dapibus in, cursus elementum ipsum.</p>
     <div class="example demo">${CalendarDemoTpl}</div>
     <code-snippet [code]="CalendarDemoTpl"></code-snippet>
 
-    <h2>Time Picker</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non massa et augue molestie lacinia. Ut purus sem, bibendum at blandit vitae, bibendum sed turpis. Fusce eros libero, fringilla non dapibus in, cursus elementum ipsum.</p>
+    <h2>Time Picker  <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/tree/master/src/elements/time-picker">(source)</a></small></h2>
+    <p>Time pickers come in 12 hour or 24 hour style.</p>
 
-    <h5>Standalone Time Picker <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/tree/master/src/elements/time-picker">(source)</a></small></h5>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non massa et augue molestie lacinia. Ut purus sem, bibendum at blandit vitae, bibendum sed turpis. Fusce eros libero, fringilla non dapibus in, cursus elementum ipsum.</p>
+    <h5>Standalone Time Picker</h5>
     <div class="example demo">${TimeDemoTpl}</div>
     <code-snippet [code]="TimeDemoTpl"></code-snippet>
 </div>
--- demo/pages/loading/LoadingDemo.js
+++ demo/pages/loading/LoadingDemo.js
@@ -15,21 +15,17 @@ const template = `
 
     <h5>Line</h5>
     <p>The Dot Line animation is indeterminate.</p>
-    <div class="example loading-circle-demo">${LoadingLineDemoTpl}</div>
+    <div class="example loading-line-demo">${LoadingLineDemoTpl}</div>
     <code-snippet [code]="LoadingLineDemoTpl"></code-snippet>
 
-    <h5>Circle</h5>
+    <h5>Spinner</h5>
     <p>
-        The Dot Circle animation is used when the animation can be triggered by data retrieval.
-        It has three stages, <code>open</code>, <code>cycle</code>, and <code>close</code> that can be triggered by it's controller.
-        Click the loading animation to cycle through the different cycles.
+        The Dot Spinner animation is used as an alternate to the loading line animation.
     </p>
-    <div class="example loading-circle-demo">${LoadingCircleDemoTpl}</div>
+    <div class="example loading-spinner-demo">${LoadingCircleDemoTpl}</div>
     <code-snippet [code]="LoadingCircleDemoTpl"></code-snippet>
 </div>
 `;
-
-const CIRCLE_STAGES = ['open', 'cycle', 'close'];
 
 @Component({
     selector: 'loading-demo',
@@ -41,17 +37,7 @@ const CIRCLE_STAGES = ['open', 'cycle', 'close'];
 })
 export class LoadingDemo {
     constructor() {
-        this.stages = 'open';
         this.LoadingCircleDemoTpl = LoadingCircleDemoTpl;
         this.LoadingLineDemoTpl = LoadingLineDemoTpl;
-    }
-
-    ngAfterContentInit() {
-        this.stages = 'cycle';
-    }
-
-    changeStage() {
-        let idx = CIRCLE_STAGES.indexOf(this.stages);
-        this.stages = (idx === CIRCLE_STAGES.length - 1) ? CIRCLE_STAGES[0] : CIRCLE_STAGES[idx + 1];
     }
 }
