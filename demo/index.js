c516f814f6a03a1be8ffc5cf2133063e28508fda
Joshua Godi <jgodi@bullhorn.com>
2016-05-16 21:08:42 -0500

    chore(imports): Fixing bad imports

--- demo/index.js
+++ demo/index.js
@@ -1,14 +1,12 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { MODAL_BROWSER_PROVIDERS } from 'angular2-modal/platform-browser';
 import 'rxjs/add/operator/map';
 
 import { DemoApp } from './pages/app/App';
 import './index.scss';
 
 bootstrap(DemoApp, [
-    ...MODAL_BROWSER_PROVIDERS,
     ...ROUTER_PROVIDERS,
     { provide: LocationStrategy, useClass: HashLocationStrategy }
 ]).catch(err => console.error(err)); // eslint-disable-line
