f1ba0ee33b281def98e1a2369f2c118375a2140b
Joshua Godi <jgodi@bullhorn.com>
2016-06-01 21:13:47 -0500

    chore(labels): Fixing labels format

--- src/services/novo-label-service.js
+++ src/services/novo-label-service.js
@@ -2,41 +2,15 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class NovoLabelService {
-    get filters() {
-        return 'Filters';
-    }
-
-    get clear() {
-        return 'Clear';
-    }
-
-    get emptyTableMessage() {
-        return 'No Matching Records';
-    }
-
-    get pickerError() {
-        return 'Oops! An error occurred.';
-    }
-
-    get pickerEmpty() {
-        return 'No results to display...';
-    }
-
-    get quickNoteError() {
-        return this.pickerError;
-    }
-
-    get quickNoteEmpty() {
-        return this.pickerEmpty;
-    }
-
-    get required() {
-        return 'Required';
-    }
-
-    get numberTooLarge() {
-        return 'Number is too large';
-    }
+    filters = 'Filters';
+    clear = 'Clear';
+    emptyTableMessage = 'No Matching Records';
+    pickerError = 'Oops! An error occurred.';
+    pickerEmpty = 'No results to display...';
+    quickNoteError = 'Oops! An error occurred.';
+    quickNoteEmpty = 'No results to display...';
+    required = 'Required';
+    numberTooLarge = 'Number is too large';
 }
 
 export const NOVO_ELEMENTS_LABELS_PROVIDERS = [
