b467f413ed9f79f499d0091e4e3ea338fea22f9f
Kyle Murray <k.murray@bullhorn.com>
2016-07-05 14:54:57 -0400

    BH-38530 Novo: Incorporate Schedule Next Action within Notes Changes to styling for Tiles component and adding form support for it

---
+++ src/elements/form/extras/tiles-input/TilesInput.js
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { COMMON_DIRECTIVES, NgModel } from '@angular/common';
+
+import { BaseInput } from './../FormExtras';
+import { NOVO_TILES_ELEMENTS } from './../../../tiles';
+import { NOVO_PICKER_ELEMENTS } from './../../../picker';
+import { NovoLabelService } from './../../../../novo-elements';
+
+@Component({
+    selector: 'tiles-input',
+    inputs: [
+        'name',
+        'placeholder',
+        'options',
+        'placeholder',
+        'required'
+    ],
+    directives: [
+        COMMON_DIRECTIVES,
+        NOVO_TILES_ELEMENTS,
+        NOVO_PICKER_ELEMENTS,
+        NgModel
+    ],
+    template: `
+        <i *ngIf="required" class="required-indicator" [ngClass]="{'bhi-circle': !control.valid, 'bhi-check': control.valid}"></i>
+        <novo-tiles [options]="options" (changed)="onChanged($event)"></novo-tiles>
+        <span class="error-message" *ngIf="required && control.touched && control?.errors?.required">{{labels.required}}</span>
+    `
+})
+export class TilesInput extends BaseInput {
+    constructor(labels:NovoLabelService) {
+        super();
+        this.labels = labels;
+    }
+
+    onChanged(e) {
+        this.value = e.value ? e.value : null;
+        this.update.emit(this.value);
+    }
+}
