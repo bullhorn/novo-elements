d20c207c8b8b617fd65ac530be2650701a6e2bd9
Kameron Sween <krsween@users.noreply.github.com>
2016-08-02 10:25:48 -0400

    feat(TipWell): adding new TipWell feature for displaying a tip dialog in-line (#129)

---
+++ src/elements/tip-well/README.md
@@ -0,0 +1,12 @@
+# TipWells
+
+## Usage
+    import { NOVO_TIPWELL_ELEMENTS } from 'novo-elements';
+
+##### Properties
+- `'name' : String`
+    * Defines the name of the TipWell that will be used by localStorage for state management.
+- `'tip' : String`
+    * The contents of the TipWell.
+- `'buttonText' : String`
+    * An override of the default 'Ok, Got it' button text.
---
+++ src/elements/tip-well/TipWell.js
@@ -0,0 +1,66 @@
+// NG2
+import { Component } from '@angular/core';
+// App
+import { NOVO_BUTTON_ELEMENTS } from '../button';
+
+@Component({
+    selector: 'novo-tip-well',
+    inputs: [
+        'name',
+        'tip',
+        'buttonText'
+    ],
+    directives: [
+        NOVO_BUTTON_ELEMENTS
+    ],
+    template: `
+        <div *ngIf="isActive">
+            <p>{{ tip }}</p>
+            <button theme="dialogue" (click)="hideTip()">{{ buttonText }}</button>
+        </div>
+    `
+})
+export class TipWell {
+    constructor() {
+        this.isActive = true;
+        // Check if localStorage is enabled
+        this.isLocalStorageEnabled = (() => {
+            let isEnabled = false;
+            if (typeof localStorage === 'object') {
+                try {
+                    localStorage.setItem('lsTest', 1);
+                    localStorage.removeItem('lsTest');
+                    isEnabled = true;
+                } catch (e) {
+                    console.warn('This web browser does not support storing settings locally. In Safari, the most common cause of this is using "Private Browsing Mode". Some settings may not save or some features may not work properly for you.'); // eslint-disable-line
+                }
+            }
+            return isEnabled;
+        })();
+    }
+
+    ngOnInit() {
+        this.tip = this.tip || '';
+        this.buttonText = this.buttonText || 'Ok, Got it';
+        // Set a (semi) unique name for the tip-well
+        this.name = this.name || Math.round(Math.round() * 100);
+        this.localStorageKey = `novo-tw_${this.name}`;
+        // Check localStorage for state
+        if (this.isLocalStorageEnabled) {
+            let storedValue = JSON.parse(localStorage.getItem(this.localStorageKey));
+            this.isActive = storedValue !== false;
+        }
+    }
+
+	/**
+     * @name hideTip
+     */
+    hideTip() {
+        if (this.isLocalStorageEnabled) {
+            localStorage.setItem(this.localStorageKey, JSON.stringify(false));
+        }
+        this.isActive = false;
+    }
+}
+
+export const NOVO_TIPWELL_ELEMENTS = [TipWell];
