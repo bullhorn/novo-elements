099ad10e94d70165590ac7bf825afe829bfd3312
Kameron Sween <krsween@users.noreply.github.com>
2016-08-04 11:19:03 -0400

    refactor(Table): Language tweak from UX (#137)

--- src/services/novo-label-service.js
+++ src/services/novo-label-service.js
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class NovoLabelService {
-    filters = 'Filters';
+    filters = 'Filter';
     clear = 'Clear';
     emptyTableMessage = 'No Matching Records';
     pickerError = 'Oops! An error occurred.';
