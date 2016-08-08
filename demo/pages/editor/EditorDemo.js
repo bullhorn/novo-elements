1050c4c9fe5f13c110503cc61196f6163316ccd1
Joshua Godi <jgodi@bullhorn.com>
2016-08-08 07:42:13 -0500

    feat(editor): Adding in a wrapper around the CKEditor

---
+++ demo/pages/editor/EditorDemo.js
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NOVO_EDITOR_ELEMENTS } from './../../../src/novo-elements';
+import { CodeSnippet } from '../../elements/codesnippet/CodeSnippet';
+
+import BasicDemoTpl from './templates/BasicEditorDemo.html';
+
+const template = `
+<div class="container">
+    <h1>CK Editor <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/tree/master/src/elements/editor">(source)</a></small></h1>
+    <p>Basic HTML editor using CK Editor.</p>
+
+    <h5>Basic</h5>
+    <div class="example editor-demo">${BasicDemoTpl}</div>
+    <code-snippet [code]="BasicDemoTpl"></code-snippet>
+</div>
+`;
+
+@Component({
+    selector: 'editor-demo',
+    template: template,
+    directives: [NOVO_EDITOR_ELEMENTS, CodeSnippet]
+})
+export class EditorDemo {
+    constructor() {
+        this.BasicDemoTpl = BasicDemoTpl;
+    }
+}
---
+++ src/elements/ckeditor/CKEditor.js
@@ -0,0 +1,91 @@
+import { Component, Input, Output, ViewChild, Optional, EventEmitter, Renderer } from '@angular/core'; // eslint-disable-line
+import { NgControl } from '@angular/common';
+
+@Component({
+    selector: 'novo-editor',
+    template: '<textarea #host (change)="onValueChange($event)"></textarea>'
+})
+export class CKEditor {
+    @Input() config;
+    @Input() ngModel;
+
+    @Output() change = new EventEmitter();
+    @ViewChild('host') host;
+
+    value = '';
+    instance;
+    ngControl;
+    renderer;
+
+    constructor(@Optional() ngControl:NgControl, renderer:Renderer) {
+        if (ngControl) {
+            ngControl.valueAccessor = this;
+            this.ngControl = ngControl;
+        }
+        this.renderer = renderer;
+    }
+
+    ngOnDestroy() {
+        if (this.instance) {
+            this.instance.removeAllListeners();
+            this.instance.destroy();
+            this.instance = null;
+        }
+    }
+
+    ngAfterViewInit() {
+        // Configuration
+        let config = this.config || {};
+        this.ckeditorInit(config);
+    }
+
+    onValueChange() {
+        let value = this.host.nativeElement.value;
+        this.change.emit(value);
+        if (this.ngControl) {
+            this.ngControl.viewToModelUpdate(value);
+        }
+    }
+
+    ckeditorInit(config) {
+        if (!CKEDITOR) {
+            console.error('Make sure to include CKEditor sources in your dependencies!'); // eslint-disable-line
+            return;
+        }
+
+        // CKEditor replace textarea
+        this.instance = CKEDITOR.replace(this.host.nativeElement, config);
+
+        // Set initial value
+        this.instance.setData(this.value);
+
+        // CKEditor change event
+        this.instance.on('change', () => {
+            this.renderer.setElementProperty(this.host.nativeElement, 'value', this.instance.getData());
+            this.renderer.invokeElementMethod(this.host.nativeElement, 'dispatchEvent', [new Event('change')]);
+        });
+    }
+
+    writeValue(value) {
+        this.value = value;
+        if (this.instance) {
+            this.instance.setData(value);
+        }
+    }
+
+    onChange() {
+    }
+
+    onTouched() {
+    }
+
+    registerOnChange(fn) {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn) {
+        this.onTouched = fn;
+    }
+}
+
+export const NOVO_EDITOR_ELEMENTS = [CKEditor];
