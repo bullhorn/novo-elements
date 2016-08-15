ad09a97ec4c07f8ddd2f4c59e7cbde432c109061
Brian Kimball <bkimball@bullhorn.com>
2016-08-15 09:16:47 -0400

    chore(tests): DatePicker and Drawer tests (more needed)

--- src/elements/date-picker/DatePicker.spec.js
+++ src/elements/date-picker/DatePicker.spec.js
@@ -1,135 +1,132 @@
-// TODO
-// import { Component } from '@angular/core';
-// import { COMMON_DIRECTIVES } from '@angular/common';
-//
-// import { DatePicker } from './DatePicker';
-// import { testComponent, grabComponent } from './../../testing/TestHelpers';
-// import moment from 'moment/moment';
-//
-// @Component({
-//     selector: 'test-cmp',
-//     directives: [DatePicker, COMMON_DIRECTIVES],
-//     template: '<novo-date-picker [(ngModel)]="date"></novo-date-picker>'
-// })
-// class TestCmp {
-//     constructor() {
-//         this.date = new Date();
-//     }
-// }
-//
-//
-// describe('Element: DatePicker', () => {
-//     let comp;
-//     beforeEachProviders(() => [
-//         DatePicker
-//     ]);
-//
-//     it('should initialize correctly', testComponent(TestCmp, (fixture) => {
-//         const { instance, element, testComponentInstance, testComponentElement } = grabComponent(fixture, DatePicker);
-//         expect(instance).toBeTruthy();
-//         expect(element).toBeTruthy();
-//         expect(testComponentInstance).toBeTruthy();
-//         expect(testComponentElement).toBeTruthy();
-//     }));
-//
-//     beforeEach(inject([DatePicker], _comp => {
-//         comp = _comp;
-//     }));
-//
-//     it('should initialize with all of its defaults.', () => {
-//         expect(comp).toBeDefined();
-//     });
-//
-//     describe('Function: select()', () => {
-//         it('should set selected', () => {
-//             expect(comp.select).toBeDefined();
-//             comp.select(event, { date: undefined });
-//             let day = { date: moment('12/08/16') };
-//             let event = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
-//             comp.select(event, day);
-//             expect(comp.selected).toBe(day.date);
-//         });
-//
-//         it('should not set default selected values for range', () => {
-//             expect(comp.select).toBeDefined();
-//             comp.range = true;
-//             expect(comp.selected).not.toBeDefined();
-//             expect(comp.selected2).not.toBeDefined();
-//         });
-//
-//         it('should set selected values for range', () => {
-//             expect(comp.select).toBeDefined();
-//             comp.select(event, { date: undefined });
-//             let day = { date: moment('12/08/16') };
-//             let day2 = { date: moment('12/10/16') };
-//             let event = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
-//             comp.range = true;
-//             comp.select(event, day);
-//             comp.select(event, day2);
-//             expect(comp.selected).toBe(day.date);
-//             expect(comp.selected2).toBe(day2.date);
-//         });
-//
-//         it('should not set selected2 value if not range', () => {
-//             expect(comp.select).toBeDefined();
-//             comp.select(event, { date: undefined });
-//             let day = { date: moment('12/08/16') };
-//             let day2 = { date: moment('12/10/16') };
-//             let event = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
-//             comp.range = false;
-//             comp.select(event, day);
-//             comp.select(event, day2);
-//             expect(comp.selected).toBe(day2.date);
-//             expect(comp.selected2).toBe(undefined);
-//         });
-//
-//         it('should swap the selected values if selected2 is before selected', () => {
-//             expect(comp.select).toBeDefined();
-//             comp.select(event, { date: undefined });
-//             let day = { date: moment('12/10/16') };
-//             let day2 = { date: moment('12/08/16') };
-//             let event = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
-//             comp.range = true;
-//             comp.select(event, day);
-//             comp.select(event, day2);
-//             expect(comp.selected2).toBe(day.date);
-//             expect(comp.selected).toBe(day2.date);
-//         });
-//
-//         it('should clear selected range on 3rd date selection', () => {
-//             expect(comp.select).toBeDefined();
-//             comp.select(event, { date: undefined });
-//             let day = { date: moment('12/08/16') };
-//             let day2 = { date: moment('12/09/16') };
-//             let day3 = { date: moment('12/10/16') };
-//             let event = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
-//             comp.range = true;
-//             comp.select(event, day);
-//             comp.select(event, day2);
-//             comp.select(event, day3);
-//             expect(comp.selected2).toBe(null);
-//             expect(comp.selected).toBe(day3.date);
-//         });
-//     });
-//
-//     describe('Function: open()', () => {
-//         it('should toggle the view to days and back', () => {
-//             expect(comp.open).toBeDefined();
-//             let event = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
-//             comp.view = 'days';
-//             comp.open(event, 'months');
-//             expect(comp.view).toBe('months');
-//             //should reset view to days if same string is passed twice
-//             comp.open(event, 'months');
-//             expect(comp.view).toBe('days');
-//         });
-//     });
-//
-//     describe('Function: writeValue()', () => {
-//         it('should set this.value', () => {
-//             expect(comp.writeValue).toBeDefined();
-//             comp.writeValue(123456);
-//             expect(comp.value).toBe(123456);
-//         });
-//     });
-// });
+import moment from 'moment/moment';
+import { NovoDatePickerElement } from './DatePicker';
+import { APP_TEST_PROVIDERS } from './../../testing/test-providers';
+
+describe('Element: NovoDatePickerElement', () => {
+    let component;
+
+    beforeEach(() => {
+        addProviders([
+            NovoDatePickerElement,
+            APP_TEST_PROVIDERS
+        ]);
+    });
+
+    beforeEach(inject([NovoDatePickerElement], _component => {
+        component = _component;
+    }));
+
+    describe('Function: ngOnInit()', () => {
+        it('should initialize correctly', () => {
+            expect(component).toBeTruthy();
+        });
+    });
+
+    describe('Function: select()', () => {
+        it('should set selected', () => {
+            expect(component.select).toBeDefined();
+            component.select(event, { date: undefined });
+            let day = { date: moment('12/08/16') };
+            let event = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
+            component.select(event, day);
+            expect(component.selected).toBe(day.date);
+        });
+
+        it('should not set default selected values for range', () => {
+            expect(component.select).toBeDefined();
+            component.range = true;
+            expect(component.selected).not.toBeDefined();
+            expect(component.selected2).not.toBeDefined();
+        });
+
+        it('should set selected values for range', () => {
+            expect(component.select).toBeDefined();
+            component.select(event, { date: undefined });
+            let day = { date: moment('12/08/16') };
+            let day2 = { date: moment('12/10/16') };
+            let event = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
+            component.range = true;
+            component.select(event, day);
+            component.select(event, day2);
+            expect(component.selected).toBe(day.date);
+            expect(component.selected2).toBe(day2.date);
+        });
+
+        it('should not set selected2 value if not range', () => {
+            expect(component.select).toBeDefined();
+            component.select(event, { date: undefined });
+            let day = { date: moment('12/08/16') };
+            let day2 = { date: moment('12/10/16') };
+            let event = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
+            component.range = false;
+            component.select(event, day);
+            component.select(event, day2);
+            expect(component.selected).toBe(day2.date);
+            expect(component.selected2).toBe(undefined);
+        });
+
+        it('should swap the selected values if selected2 is before selected', () => {
+            expect(component.select).toBeDefined();
+            component.select(event, { date: undefined });
+            let day = { date: moment('12/10/16') };
+            let day2 = { date: moment('12/08/16') };
+            let event = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
+            component.range = true;
+            component.select(event, day);
+            component.select(event, day2);
+            expect(component.selected2).toBe(day.date);
+            expect(component.selected).toBe(day2.date);
+        });
+
+        it('should clear selected range on 3rd date selection', () => {
+            expect(component.select).toBeDefined();
+            component.select(event, { date: undefined });
+            let day = { date: moment('12/08/16') };
+            let day2 = { date: moment('12/09/16') };
+            let day3 = { date: moment('12/10/16') };
+            let event = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
+            component.range = true;
+            component.select(event, day);
+            component.select(event, day2);
+            component.select(event, day3);
+            expect(component.selected2).toBe(null);
+            expect(component.selected).toBe(day3.date);
+        });
+    });
+
+    describe('Function: open()', () => {
+        it('should toggle the view to days and back', () => {
+            expect(component.open).toBeDefined();
+            let event = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
+            component.view = 'days';
+            component.open(event, 'months');
+            expect(component.view).toBe('months');
+            //should reset view to days if same string is passed twice
+            component.open(event, 'months');
+            expect(component.view).toBe('days');
+        });
+    });
+
+    describe('Function: writeValue()', () => {
+        it('should be defined.', () => {
+            expect(component.writeValue).toBeDefined();
+        });
+
+        it('should change the value', () => {
+            component.writeValue(10);
+            expect(component.model).toBe(10);
+        });
+    });
+
+    describe('Function: registerOnChange()', () => {
+        it('should be defined.', () => {
+            expect(component.registerOnChange).toBeDefined();
+        });
+    });
+
+    describe('Function: registerOnTouched()', () => {
+        it('should be defined.', () => {
+            expect(component.registerOnTouched).toBeDefined();
+        });
+    });
+});
--- src/elements/drawer/Drawer.spec.js
+++ src/elements/drawer/Drawer.spec.js
@@ -1,1 +1,92 @@
-// TODO
+import { ElementRef } from '@angular/core';
+import { NovoDrawerElement, NovoDrawerToggleElement } from './Drawer';
+import { APP_TEST_PROVIDERS } from './../../testing/test-providers';
+
+describe('Element: NovoDrawerElement', () => {
+    let component;
+    let MockElement = new ElementRef(document.createElement('div'));
+    //let FakeEvent;
+
+    beforeEach(() => {
+        addProviders([
+            NovoDrawerElement,
+            APP_TEST_PROVIDERS
+        ]);
+    });
+
+    beforeEach(inject([NovoDrawerElement], _component => {
+        component = _component;
+        component.drawerToggle = { el: MockElement };
+        //FakeEvent = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
+    }));
+
+    describe('Function: ngOnInit()', () => {
+        it('should initialize properly', () => {
+            expect(component.ngOnInit).toBeDefined();
+            expect(component.onDrawerToggle).toBeDefined();
+            component.ngOnInit();
+            expect(component.autoClose).toBe('outsideClick');
+            expect(component.position).toBe('left');
+        });
+    });
+
+    describe('Function: toggle(open)', () => {
+        it('should open or close the drawer', () => {
+            //expect(component.toggle).toBeDefined();
+            let test = component.toggle(false);
+            expect(test).toBeTruthy();
+            test = component.toggle();
+            expect(test).toBeFalsy();
+        });
+    });
+
+    describe('Function: focusToggleElement()', () => {
+        it('should focus the toggleElement', () => {
+            expect(component.focusToggleElement).toBeDefined();
+            spyOn(component.toggleEl.nativeElement, 'focus');
+            component.focusToggleElement();
+            expect(component.toggleEl.nativeElement.focus).toHaveBeenCalled();
+        });
+    });
+});
+
+describe('Element: NovoDrawerToggleElement', () => {
+    let component;
+    let FakeEvent;
+
+    beforeEach(() => {
+        addProviders([
+            NovoDrawerElement,
+            NovoDrawerToggleElement,
+            APP_TEST_PROVIDERS
+        ]);
+    });
+
+    beforeEach(inject([NovoDrawerToggleElement], _component => {
+        component = _component;
+        FakeEvent = { preventDefault: jasmine.createSpy(), stopPropagation: jasmine.createSpy() };
+    }));
+
+    describe('Function: ngOnInit()', () => {
+        it('should initialize properly', () => {
+            expect(component.ngOnInit).toBeDefined();
+            expect(component.disabled).toBeFalsy();
+        });
+    });
+
+    describe('Function: toggleDrawer(event)', () => {
+        it('should toggle drawer', () => {
+            expect(component.toggleDrawer).toBeDefined();
+            spyOn(component.drawer, 'toggle');
+            component.toggleDrawer(FakeEvent);
+            expect(component.drawer.toggle).toHaveBeenCalled();
+        });
+    });
+
+    describe('Getter: isOpen()', () => {
+        it('should toggle isOpen value', () => {
+            let test = component.isOpen;
+            expect(test).toBeFalsy();
+        });
+    });
+});
