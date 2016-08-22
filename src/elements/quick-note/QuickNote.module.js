8cfc52e9d8721ed7ba8085a6518fb1ee28ab51a2
Joshua Godi <jgodi@bullhorn.com>
2016-08-22 10:09:18 -0500

    fix(quicknote): Adding missing module

--- src/elements/quick-note/QuickNote.module.js
+++ src/elements/quick-note/QuickNote.module.js
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 // APP
-import { NovoLoadingModule } from './../../novo-elements';
+import { NovoLoadingModule, NovoListModule } from './../../novo-elements';
 import { QuickNoteElement } from './QuickNote';
 import { QuickNoteResults } from './extras/quick-note-results/QuickNoteResults';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, NovoLoadingModule],
+    imports: [CommonModule, FormsModule, NovoLoadingModule, NovoListModule],
     declarations: [QuickNoteElement, QuickNoteResults],
     exports: [QuickNoteElement, QuickNoteResults]
 })
