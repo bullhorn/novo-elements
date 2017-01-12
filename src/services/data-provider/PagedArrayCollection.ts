c3f8bace858f7c61cf847eb365f0093e5dd98bd9
Brian Kimball <bkimball@bullhorn.com>
2017-01-12 14:39:43 -0500

    chore(): removing console log

--- src/services/data-provider/PagedArrayCollection.ts
+++ src/services/data-provider/PagedArrayCollection.ts
@@ -63,7 +63,6 @@ export class PagedArrayCollection<T> extends ArrayCollection<T> implements Paged
 
     refresh(): void {
         this.filterData = this.source.slice();
-        console.log('Filter is', this._filter);
         for (let item of this._sort.reverse()) {
             this.sortOn(item.field, item.reverse);
         }
