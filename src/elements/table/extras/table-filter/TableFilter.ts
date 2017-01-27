d6f4b185001e12e864ce3545ea68be33412d9463
Joshua Godi <jgodi@bullhorn.com>
2017-01-27 14:04:24 -0600

    fix(table): Allow filters to have enter and close

--- src/elements/table/extras/table-filter/TableFilter.ts
+++ src/elements/table/extras/table-filter/TableFilter.ts
@@ -1,12 +1,11 @@
 // NG2
-import { Directive, EventEmitter, ElementRef, Renderer, OnChanges, OnInit, Input, Output, SimpleChanges } from '@angular/core';
+import { Directive, EventEmitter, ElementRef, Renderer, OnChanges, OnInit, Input, Output, SimpleChanges, HostListener } from '@angular/core';
+// APP
+import { KeyCodes } from './../../../../utils/key-codes/KeyCodes';
+import { Helpers } from './../../../../utils/Helpers';
 
 @Directive({
     selector: '[novoTableFilter]',
-    host: {
-        '(input)': 'onChangeFilter($event)',
-        '(click)': 'onClick($event)'
-    }
 })
 export class TableFilter implements OnInit, OnChanges {
     @Input('novoTableFilter') config: any;
@@ -33,18 +32,22 @@ export class TableFilter implements OnInit, OnChanges {
         this.renderer.setElementProperty(this.element, 'value', label);
     }
 
-    onChangeFilter(event) {
+    @HostListener('keydown', ['$event'])
+    public onChangeFilter(event: KeyboardEvent) {
         clearTimeout(this.filterThrottle);
-        this.filterThrottle = setTimeout(() => {
-            this.config.filter = event.target.value;
+        if (KeyCodes.ENTER === event.keyCode) {
+            this.config.filter = (event.target as any).value;
             this.onFilterChange.emit({ filtering: this.config });
-        }, 300);
+        } else {
+            this.filterThrottle = setTimeout(() => {
+                this.config.filter = (event.target as any).value;
+                this.onFilterChange.emit({ filtering: this.config });
+            }, 300);
+        }
     }
 
-    onClick(event) {
-        if (event) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
+    @HostListener('click', ['$event'])
+    public onClick(event) {
+        Helpers.swallowEvent(event);
     }
 }
