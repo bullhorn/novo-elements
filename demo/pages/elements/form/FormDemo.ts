1dc19c0d1e115a3b5f061ca592ac470f895f2172
Kyle Murray <k.murray@bullhorn.com>
2017-02-22 10:51:49 -0500

    feat(Form): Add ability to have custom components used with the dynamic form

--- demo/pages/elements/form/FormDemo.ts
+++ demo/pages/elements/form/FormDemo.ts
@@ -21,13 +21,54 @@ import {
 
 const template = `
 <div class="container">
-
+    <h1>Forms <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/blob/master/src/elements/form">(source)</a></small></h1>
+    <p>Forms use inputs and labels to submit user content. But you already knew that. What you may not know is that our forms come in two styles 'Static' and 'Dynamic'</p>
+    
+    <h2>Static Form</h2>
+    <p>Static forms <code>&lt;novo-form /&gt;</code>.
+    
+    <h5>Textbox Based Controls</h5>
+    <div class="example form-demo">${TextBasedControlsDemoTpl}</div>
+    <code-snippet [code]="TextBasedControlsDemoTpl"></code-snippet>
+    
+    <h5>Checkbox Controls</h5>
+    <div class="example form-demo">${CheckBoxControlsDemoTpl}</div>
+    <code-snippet [code]="CheckBoxControlsDemoTpl"></code-snippet>
+    
+    <h5>File Input Controls</h5>
+    <div class="example form-demo">${FileInputControlsDemoTpl}</div>
+    <code-snippet [code]="FileInputControlsDemoTpl"></code-snippet>
+    
+    <h5>Calendar Controls</h5>
+    <div class="example form-demo">${CalendarControlsDemoTpl}</div>
+    <code-snippet [code]="CalendarControlsDemoTpl"></code-snippet>
+    
+    <h5>Picker Controls</h5>
+    <div class="example form-demo">${PickerControlsDemoTpl}</div>
+    <code-snippet [code]="PickerControlsDemoTpl"></code-snippet>
+    
     <h2>Dynamic Form</h2>
     <p>Dynamic forms are composed of one element, <code>&lt;novo-dynamic-form [controls]="controls"/&gt;</code> and allow you to pass in the controls and form and it will create the form for you.</p>
-
+    
     <h5>Basic</h5>
     <div class="example form-demo dynamic">${DynamicFormDemoTpl}</div>
     <code-snippet [code]="DynamicFormDemoTpl"></code-snippet>
+    
+    <h5>Vertical</h5>
+    <div class="example form-demo dynamic">${VerticalDynamicFormDemoTpl}</div>
+    <code-snippet [code]="VerticalDynamicFormDemoTpl"></code-snippet>
+    
+    <h5>Fieldsets</h5>
+    <div class="example form-demo fieldsets">${FieldsetsFormDemoTpl}</div>
+    <code-snippet [code]="FieldsetsFormDemoTpl"></code-snippet>
+    
+    <h5>Updating Fields/Status</h5>
+    <div class="example form-demo updating">${UpdatingFormDemoTpl}</div>
+    <code-snippet [code]="UpdatingFormDemoTpl"></code-snippet>
+    
+    <h5>Field Interactions</h5>
+    <div class="example form-demo field-interactions">${FieldInteractionTpl}</div>
+    <code-snippet [code]="FieldInteractionTpl"></code-snippet>
 </div>
 `;
 
