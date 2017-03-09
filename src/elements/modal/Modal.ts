8a6269e4b4c7831a57afbe652c31f8b9439cb110
Nathan Dickerson <nathan@edickerson.net>
2017-03-08 22:29:15 -0600

    feat(modal): Added cancel event to the Notification Model (#432)

--- src/elements/modal/Modal.ts
+++ src/elements/modal/Modal.ts
@@ -1,5 +1,5 @@
 // NG2
-import { Component, ViewContainerRef, ViewChild, AfterViewInit, Input, OnInit } from '@angular/core';
+import { Component, ViewContainerRef, ViewChild, AfterViewInit, Input, Output, EventEmitter, OnInit } from '@angular/core';
 // APP
 import { Deferred } from './../../utils/deferred/Deferred';
 import { ComponentUtils } from './../../utils/component-utils/ComponentUtils';
@@ -106,6 +106,8 @@ export class NovoModalNotificationElement implements OnInit {
     @Input() type: string;
     @Input() icon: string;
 
+    @Output() cancel: EventEmitter<any> = new EventEmitter();
+
     iconType: string;
 
     constructor(private modalRef: NovoModalRef) {
@@ -113,6 +115,7 @@ export class NovoModalNotificationElement implements OnInit {
     }
 
     close() {
+        this.cancel.emit();
         this.modalRef.close();
     }
 
