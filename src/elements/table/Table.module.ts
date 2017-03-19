64bce7190d56acc42b3f652d472e03e0a5044d03
krsween <kameron@krsween.com>
2017-03-19 09:04:26 -0500

    chore(testing): Cleaning up Table module

--- src/elements/table/Table.module.ts
+++ src/elements/table/Table.module.ts
@@ -3,21 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 // APP
-import { NovoButtonModule } from './../button/Button.module';
-import { NovoToastModule } from './../toast/Toast.module';
-import { NovoTooltipModule } from './../tooltip/Tooltip.module';
-import { NovoDropdownModule } from './../dropdown/Dropdown.module';
-import { NovoFormModule } from './../form/Form.module';
-import { NovoLoadingModule } from './../loading/Loading.module';
-import { NovoDatePickerModule } from './../date-picker/DatePicker.module';
+import { NovoButtonModule } from '../button/Button.module';
+import { NovoToastModule } from '../toast/Toast.module';
+import { NovoTooltipModule } from '../tooltip/Tooltip.module';
+import { NovoDropdownModule } from '../dropdown/Dropdown.module';
+import { NovoFormModule } from '../form/Form.module';
+import { NovoLoadingModule } from '../loading/Loading.module';
+import { NovoDatePickerModule } from '../date-picker/DatePicker.module';
 import { NovoTableExtrasModule } from './extras/TableExtras.module';
-import { NovoTableActionsElement, NovoTableElement, NovoTableHeaderElement, NovoTableFooterElement, NovoTableKeepFilterFocus } from './Table';
-import { NovoFormExtrasModule } from './../form/extras/FormExtras.module';
+import { NovoTableElement } from './Table';
+import { NovoFormExtrasModule } from '../form/extras/FormExtras.module';
 
 @NgModule({
     imports: [CommonModule, FormsModule, NovoFormModule, NovoTableExtrasModule, NovoToastModule, NovoButtonModule, NovoTooltipModule, NovoDropdownModule, NovoLoadingModule, NovoDatePickerModule, NovoFormExtrasModule],
-    declarations: [NovoTableActionsElement, NovoTableElement, NovoTableHeaderElement, NovoTableFooterElement, NovoTableKeepFilterFocus],
-    exports: [NovoTableActionsElement, NovoTableElement, NovoTableHeaderElement, NovoTableFooterElement, NovoTableKeepFilterFocus]
+    declarations: [NovoTableElement],
+    exports: [NovoTableElement]
 })
 export class NovoTableModule {
 }
