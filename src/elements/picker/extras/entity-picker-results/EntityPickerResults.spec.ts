6a5823f7e3d44e8227afa1cf99bd1cf9838f3c76
Brendan Boyle <brendanboyle87@gmail.com>
2017-04-04 13:48:34 -0400

    chore(testing): Commenting back a few unit tests

--- src/elements/picker/extras/entity-picker-results/EntityPickerResults.spec.ts
+++ src/elements/picker/extras/entity-picker-results/EntityPickerResults.spec.ts
@@ -1,121 +1,135 @@
-// // APP
-// import { EntityPickerResults } from './EntityPickerResults';
-// import { APP_TEST_PROVIDERS } from './../../../../testing/test-providers';
+// NG2
+import { TestBed, async } from '@angular/core/testing';
+// APP
+import { EntityPickerResult } from './EntityPickerResults';
+import { NovoLabelService } from '../../../../services/novo-label-service';
+import { NovoLoadingElement } from '../../../loading/Loading';
+import { NovoListElement, NovoListItemElement, NovoItemAvatarElement, NovoItemHeaderElement, NovoItemTitleElement, NovoItemContentElement } from '../../../list/List';
 
-// describe('Component: EntityPickerResults', () => {
-//     let comp;
+describe('Elements: EntityPickerResult', () => {
+    let fixture;
+    let component;
 
-//     beforeEach(() => {
-//         addProviders([
-//             EntityPickerResults,
-//             APP_TEST_PROVIDERS
-//         ]);
-//     });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                EntityPickerResult,
+                NovoLoadingElement,
+                NovoListElement,
+                NovoListItemElement,
+                NovoItemAvatarElement,
+                NovoItemContentElement,
+                NovoItemHeaderElement,
+                NovoItemTitleElement
+            ],
+            providers: [
+                { provide: NovoLabelService, useClass: NovoLabelService }
+            ]
+        }).compileComponents();
+        fixture = TestBed.createComponent(EntityPickerResult);
+        component = fixture.debugElement.componentInstance;
+    }));
 
-//     beforeEach(inject([EntityPickerResults], _comp => {
-//         comp = _comp;
-//     }));
+    it('should initialize correctly', () => {
+        expect(component).toBeDefined();
+    });
 
-//     it('should initialize correctly', () => {
-//         expect(comp).toBeDefined();
-//     });
+    describe('Method: getIconForResult(result)', () => {
+        it('should return the right icon name for ClientContact', () => {
+            expect(component.getIconForResult({ searchEntity: 'ClientContact' })).toBe('person contact');
+        });
+        it('should return the right icon name for ClientCorporation', () => {
+            expect(component.getIconForResult({ searchEntity: 'ClientCorporation' })).toBe('company');
+        });
+        it('should return the right icon name for Opportunity', () => {
+            expect(component.getIconForResult({ searchEntity: 'Opportunity' })).toBe('opportunity');
+        });
+        it('should return the right icon name for Candidate', () => {
+            expect(component.getIconForResult({ searchEntity: 'Candidate' })).toBe('candidate');
+        });
+        it('should return the right icon name for Lead', () => {
+            expect(component.getIconForResult({ searchEntity: 'Lead' })).toBe('lead');
+        });
+        it('should return the right icon name for JobOrder', () => {
+            expect(component.getIconForResult({ searchEntity: 'JobOrder' })).toBe('job');
+        });
+        it('should return the right icon name for Placement', () => {
+            expect(component.getIconForResult({ searchEntity: 'Placement' })).toBe('star placement');
+        });
+        it('should return empty string for unrecognized entity names', () => {
+            expect(component.getIconForResult({ searchEntity: 'Unknown' })).toBe('');
+        });
+    });
 
-//     describe('Method: getIconForResult(result)', () => {
-//         it('should return the right icon name for ClientContact', () => {
-//             expect(comp.getIconForResult({ searchEntity: 'ClientContact' })).toBe('person contact');
-//         });
-//         it('should return the right icon name for ClientCorporation', () => {
-//             expect(comp.getIconForResult({ searchEntity: 'ClientCorporation' })).toBe('company');
-//         });
-//         it('should return the right icon name for Opportunity', () => {
-//             expect(comp.getIconForResult({ searchEntity: 'Opportunity' })).toBe('opportunity');
-//         });
-//         it('should return the right icon name for Candidate', () => {
-//             expect(comp.getIconForResult({ searchEntity: 'Candidate' })).toBe('candidate');
-//         });
-//         it('should return the right icon name for Lead', () => {
-//             expect(comp.getIconForResult({ searchEntity: 'Lead' })).toBe('lead');
-//         });
-//         it('should return the right icon name for JobOrder', () => {
-//             expect(comp.getIconForResult({ searchEntity: 'JobOrder' })).toBe('job');
-//         });
-//         it('should return the right icon name for Placement', () => {
-//             expect(comp.getIconForResult({ searchEntity: 'Placement' })).toBe('star placement');
-//         });
-//         it('should return empty string for unrecognized entity names', () => {
-//             expect(comp.getIconForResult({ searchEntity: 'Unknown' })).toBe('');
-//         });
-//     });
-
-//     describe('Method: getNameForResult(result)', () => {
-//         it('should return a correctly formatted name for ClientContact', () => {
-//             let input = {
-//                 searchEntity: 'ClientContact',
-//                 firstName: 'James',
-//                 lastName: 'Bond'
-//             };
-//             expect(comp.getNameForResult(input)).toBe('James Bond');
-//         });
-//         it('should return a correctly formatted name for ClientCorporation', () => {
-//             let input = {
-//                 searchEntity: 'ClientCorporation',
-//                 name: 'Bottles, Inc.'
-//             };
-//             expect(comp.getNameForResult(input)).toBe('Bottles, Inc.');
-//         });
-//         it('should return a correctly formatted name for Opportunity', () => {
-//             let input = {
-//                 searchEntity: 'Opportunity',
-//                 title: 'Lead Bottlemaker'
-//             };
-//             expect(comp.getNameForResult(input)).toBe('Lead Bottlemaker');
-//         });
-//         it('should return a correctly formatted name for Lead', () => {
-//             let input = {
-//                 searchEntity: 'Lead',
-//                 firstName: 'James',
-//                 lastName: 'Bond'
-//             };
-//             expect(comp.getNameForResult(input)).toBe('James Bond');
-//         });
-//         it('should return a correctly formatted name for Candidate', () => {
-//             let input = {
-//                 searchEntity: 'Candidate',
-//                 firstName: 'James',
-//                 lastName: 'Bond'
-//             };
-//             expect(comp.getNameForResult(input)).toBe('James Bond');
-//         });
-//         it('should return a correctly formatted name for Job Order', () => {
-//             let input = {
-//                 searchEntity: 'JobOrder',
-//                 title: 'Mock Job Title'
-//             };
-//             expect(comp.getNameForResult(input)).toBe('Mock Job Title');
-//         });
-//         it('should return a correctly formatted name for Placement', () => {
-//             let input = {
-//                 searchEntity: 'Placement',
-//                 candidate: {
-//                     firstName: 'James',
-//                     lastName: 'Bond'
-//                 }
-//             };
-//             expect(comp.getNameForResult(input)).toBe('James Bond');
-//         });
-//         it('should return the name when present for an unknown entity', () => {
-//             let input = {
-//                 searchEntity: 'Unknown',
-//                 name: 'James Bond'
-//             };
-//             expect(comp.getNameForResult(input)).toBe('James Bond');
-//         });
-//         it('should return an empty string when name is not present for an unknown entity', () => {
-//             let input = {
-//                 searchEntity: 'Unknown',
-//                 title: 'Mock Job Title'
-//             };
-//             expect(comp.getNameForResult(input)).toBe('');
-//         });
-//     });
-// });
+    describe('Method: getNameForResult(result)', () => {
+        it('should return a correctly formatted name for ClientContact', () => {
+            let input = {
+                searchEntity: 'ClientContact',
+                firstName: 'James',
+                lastName: 'Bond'
+            };
+            expect(component.getNameForResult(input)).toBe('James Bond');
+        });
+        it('should return a correctly formatted name for ClientCorporation', () => {
+            let input = {
+                searchEntity: 'ClientCorporation',
+                name: 'Bottles, Inc.'
+            };
+            expect(component.getNameForResult(input)).toBe('Bottles, Inc.');
+        });
+        it('should return a correctly formatted name for Opportunity', () => {
+            let input = {
+                searchEntity: 'Opportunity',
+                title: 'Lead Bottlemaker'
+            };
+            expect(component.getNameForResult(input)).toBe('Lead Bottlemaker');
+        });
+        it('should return a correctly formatted name for Lead', () => {
+            let input = {
+                searchEntity: 'Lead',
+                firstName: 'James',
+                lastName: 'Bond'
+            };
+            expect(component.getNameForResult(input)).toBe('James Bond');
+        });
+        it('should return a correctly formatted name for Candidate', () => {
+            let input = {
+                searchEntity: 'Candidate',
+                firstName: 'James',
+                lastName: 'Bond'
+            };
+            expect(component.getNameForResult(input)).toBe('James Bond');
+        });
+        it('should return a correctly formatted name for Job Order', () => {
+            let input = {
+                searchEntity: 'JobOrder',
+                title: 'Mock Job Title'
+            };
+            expect(component.getNameForResult(input)).toBe('Mock Job Title');
+        });
+        it('should return a correctly formatted name for Placement', () => {
+            let input = {
+                searchEntity: 'Placement',
+                candidate: {
+                    firstName: 'James',
+                    lastName: 'Bond'
+                }
+            };
+            expect(component.getNameForResult(input)).toBe('James Bond');
+        });
+        it('should return the name when present for an unknown entity', () => {
+            let input = {
+                searchEntity: 'Unknown',
+                name: 'James Bond'
+            };
+            expect(component.getNameForResult(input)).toBe('James Bond');
+        });
+        it('should return an empty string when name is not present for an unknown entity', () => {
+            let input = {
+                searchEntity: 'Unknown',
+                title: 'Mock Job Title'
+            };
+            expect(component.getNameForResult(input)).toBe('');
+        });
+    });
+});
