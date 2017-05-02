0219cb5d17df9cace3e54c86325bdef98a7d6155
Joshua Godi <jgodi@bullhorn.com>
2017-05-02 00:44:46 -0500

    chore(demo): Adding demo back

--- demo/pages/elements/calendar/CalendarDemo.ts
+++ demo/pages/elements/calendar/CalendarDemo.ts
@@ -10,9 +10,22 @@ let DateTimeDemoTpl = require('./templates/DateTimeDemo.html');
 
 const template = `
 <div class="container">
+    <h1>Date and Time Pickers</h1>
+    <p>These allow users to easily select a time and date. It comes in a handful of varieties based on the content of the field.</p>
+
+    <h2>Calendar Picker  <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/blob/master/src/elements/date-picker">(source)</a></small></h2>
+    <p>The calendar picker is used to select a date. It appears in all date picker fields.</p>
+
+    <h5>Big Calendar Picker</h5>
+    <div class="example demo">${BigCalendarDemoTpl}</div>
+    <code-snippet [code]="BigCalendarDemoTpl"></code-snippet>
+
     <h5>Full Calendar Picker</h5>
     <div class="example demo">${CalendarDemoTpl}</div>
     <code-snippet [code]="CalendarDemoTpl"></code-snippet>
+
+    <h2>Time Picker  <small><a target="_blank" href="https://github.com/bullhorn/novo-elements/blob/master/src/elements/time-picker">(source)</a></small></h2>
+    <p>Time pickers come in 12 hour or 24 hour style.</p>
 
     <h5>Standalone Time Picker</h5>
     <div class="example demo">${TimeDemoTpl}</div>
@@ -58,10 +71,10 @@ export class CalendarDemoComponent {
     RangeDemoTpl: string = RangeDemoTpl;
     DateTimeDemoTpl: string = DateTimeDemoTpl;
 
-    time: Date = new Date(1493096913317);
-    dateOne: Date = new Date(1493096913317);
-    dateTwo: Date = new Date(1493096913317);
-    dateTime: Date = new Date(1493096913317);
+    time: Date = new Date();
+    dateOne: Date = new Date();
+    dateTwo: Date = new Date();
+    dateTime: Date = new Date();
     start: any = new Date().setMonth(new Date().getMonth() - 1);
     end: any = new Date().setMonth(new Date().getMonth() + 1);
     value: any = {
