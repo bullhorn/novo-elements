0677ebf82779c4c6d789052ba4b935045b344ccb
Joshua Godi <jgodi@bullhorn.com>
2017-06-23 08:41:25 -0500

    more files

---
+++ src/elements/table/extras/percentage-cell/PercentageCell.ts
@@ -0,0 +1,13 @@
+// NG2
+import { Component } from '@angular/core';
+// APP
+import { BaseRenderer } from '../base-renderer/BaseRenderer';
+
+@Component({
+    selector: 'percentage-cell',
+    template: `
+        <div class="percentage" *ngIf="value || value === 0">{{ value | percent:'1.2-2' }}</div>
+    `
+})
+export class PercentageCell extends BaseRenderer {
+}
