a683e770c7083d2a5eb43aca86653ff94e7eeda2
Joshua Godi <jgodi@bullhorn.com>
2017-06-30 10:32:43 -0500

    test

--- testing.js
+++ testing.js
@@ -1,6 +1,6 @@
 var simpleGit = require('simple-git');
 
-simpleGit().diffSummary('HEAD^', (err, data) => {
+simpleGit().diffSummary(['HEAD', 'master'], (err, data) => {
     if (err) {
         throw err;
     }
