036a556efced00e6d9f3b3efefd25bc645fd9148
Elizabeth Carretto <escarretto@gmail.com>
2017-08-11 10:16:32 -0500

    fix(form): fix the setting of readOnly

--- src/elements/form/NovoFormControl.ts
+++ src/elements/form/NovoFormControl.ts
@@ -109,9 +109,9 @@ export class NovoFormControl extends FormControl {
     public setReadOnly(isReadOnly: boolean): void {
         this.readOnly = isReadOnly;
         if (this.readOnly) {
-            this.enable();
-        } else {
             this.disable();
+        } else {
+            this.enable();
         }
     }
 
