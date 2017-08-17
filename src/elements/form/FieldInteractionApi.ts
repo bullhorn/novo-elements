8d09a0d19a06afdc1c5d370fb9111001f865a3b3
Joshua Godi <jgodi@bullhorn.com>
2017-08-17 14:56:01 -0500

    more

--- src/elements/form/FieldInteractionApi.ts
+++ src/elements/form/FieldInteractionApi.ts
@@ -395,7 +395,7 @@ export class FieldInteractionApi {
     public modifyPickerConfig(key: string, config: { format?: string, optionsUrl?: string, optionsUrlBuilder?: Function, optionsPromise?: any, options?: any[] }, mapper?: Function): void {
         let control = this.getControl(key);
         if (control) {
-            let newConfig: any = Object.assign({}, control.config);;
+            let newConfig: any = Object.assign({}, control.config);
             if (config.optionsUrl || config.optionsUrlBuilder || config.optionsPromise) {
                 newConfig = {
                     format: config.format,
