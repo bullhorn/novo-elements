886e853f4367d41c87f576101ccc2a42325140bf
Joshua Godi <joshuagodi@gmail.com>
2017-12-28 09:46:45 -0600

    chore(cleanup): Removing dead code

--- src/platform/utils/calendar-utils/CalendarInterfaces.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CalendarEventResponse;
-(function (CalendarEventResponse) {
-    CalendarEventResponse[CalendarEventResponse["Maybe"] = 0] = "Maybe";
-    CalendarEventResponse[CalendarEventResponse["Accepted"] = 1] = "Accepted";
-    CalendarEventResponse[CalendarEventResponse["Rejected"] = 2] = "Rejected";
-})(CalendarEventResponse = exports.CalendarEventResponse || (exports.CalendarEventResponse = {}));
-//# sourceMappingURL=CalendarInterfaces.js.map
