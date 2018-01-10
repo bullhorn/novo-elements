6f58902e05059cfe20c345177a893a6fa7e8c0b2
Drew Dudley <drewdudley93@gmail.com>
2018-01-10 12:54:36 -0600

    feat(EntityUtils): Adding Tearsheet to entity names

--- src/platform/utils/entity-utils/EntityUtils.ts
+++ src/platform/utils/entity-utils/EntityUtils.ts
@@ -56,6 +56,7 @@ export class EntityUtils {
             submission: 'JobSubmission',
             references: 'CandidateReference',
             appointment: 'Appointment',
+            tearsheet: 'Tearsheet',
         };
     }
 
