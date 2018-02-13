b3dbc8706ebff6d24d2549becfe2333972a4eab3
Drew Dudley <drew.dudley@bullhorn.com>
2018-02-13 09:06:07 -0600

    BH-54679 adding entity list module

---
+++ src/platform/elements/entity-list/EntityList.module.ts
@@ -0,0 +1,12 @@
+// NG2
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { EntityList } from './EntityList';
+
+@NgModule({
+    imports: [CommonModule],
+    exports: [EntityList],
+    declarations: [EntityList]
+})
+export class NovoEntityListModule {
+}
