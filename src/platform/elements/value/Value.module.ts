1524077111d7afe2a50320cca0c7d63eac103fea
Drew Dudley <drewdudley93@gmail.com>
2018-02-14 12:12:37 -0600

    feat(entity-list): Adding entity list preview to value

--- src/platform/elements/value/Value.module.ts
+++ src/platform/elements/value/Value.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 // APP
 import { NovoValueElement, NOVO_VALUE_THEME, NOVO_VALUE_TYPE } from './Value';
 import { RenderPipe } from './Render';
+import { EntityList } from './EntityList'
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [NovoValueElement, RenderPipe],
-    exports: [NovoValueElement, RenderPipe]
+    declarations: [NovoValueElement, RenderPipe, EntityList],
+    exports: [NovoValueElement, RenderPipe, EntityList]
 })
 export class NovoValueModule {
 }
