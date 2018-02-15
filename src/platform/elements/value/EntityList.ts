a82fc21ccde1f6d98d0f479273ff41d3fd2a101b
Drew Dudley <drew.dudley@bullhorn.com>
2018-02-15 08:59:19 -0600

    BH-54679 adding entity colored dot to entity list in value

--- src/platform/elements/value/EntityList.ts
+++ src/platform/elements/value/EntityList.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } 
     template: `
         <div *ngFor="let entity of data.data" class="entity">
             <a *ngIf="isLinkable(entity)" (click)="openLink(entity)">
-                {{ entity | render : meta }}
+                <i class="bhi-circle {{getClass(entity)}}"></i>{{ entity | render : meta }}
             </a>
             <span *ngIf="!isLinkable(entity)">
                 {{ entity | render : meta }}
@@ -19,6 +19,37 @@ export class EntityList implements OnInit {
     @Input() data: any;
     @Input() meta: any;
     baseEntity: string = '';
+    ENTITY_SHORT_NAMES: any = {
+        Lead: 'lead',
+        ClientContact: 'contact',
+        ClientContact1: 'contact',
+        ClientContact2: 'contact',
+        ClientContact3: 'contact',
+        ClientContact4: 'contact',
+        ClientContact5: 'contact',
+        ClientCorporation: 'company',
+        ClientCorporation1: 'company',
+        ClientCorporation2: 'company',
+        ClientCorporation3: 'company',
+        ClientCorporation4: 'company',
+        ClientCorporation5: 'company',
+        Opportunity: 'opportunity',
+        Task: 'task',
+        Note: 'note',
+        CorporateUser: 'user',
+        Candidate: 'candidate',
+        JobOrder: 'job',
+        JobOrder1: 'job',
+        JobOrder2: 'job',
+        JobOrder3: 'job',
+        JobOrder4: 'job',
+        JobOrder5: 'job',
+        Placement: 'placement',
+        JobSubmission: 'submission',
+        CandidateReference: 'references',
+        DistributionList: 'distributionList',
+        Appointment: 'appointment',
+    };
 
     constructor() {
     }
@@ -26,6 +57,10 @@ export class EntityList implements OnInit {
     ngOnInit(): any {
         this.meta.type = 'TO_ONE';
         this.baseEntity = this.meta.associatedEntity.entity;
+    }
+
+    getClass(entity: any): any {
+        return this.ENTITY_SHORT_NAMES[entity.personSubtype];
     }
 
     openLink(entity: any): void {
