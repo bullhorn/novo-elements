f9b78409f46949ffa7245e08f3fd21099a98d406
Tanvi Gadre <tgadre@bullhorn.com>
2018-02-26 15:46:37 -0500

    Adding google search api to address fields

--- src/platform/elements/form/extras/address/Address.ts
+++ src/platform/elements/form/extras/address/Address.ts
@@ -4,6 +4,7 @@ import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 // APP
 import { getCountries, getStates, getStateObjects, findByCountryName, findByCountryId } from '../../../../utils/countries/Countries';
 import { NovoLabelService } from '../../../../services/novo-label-service';
+import { GooglePlacesService } from '../../../places/places.service';
 
 // Value accessor for the component (supports ngModel)
 const ADDRESS_VALUE_ACCESSOR = {
@@ -16,6 +17,13 @@ const ADDRESS_VALUE_ACCESSOR = {
     selector: 'novo-address',
     providers: [ADDRESS_VALUE_ACCESSOR],
     template: `
+        <input id="autocomplete" placeholder="Enter your address" [(ngModel)]="searchTerm" (ngModelChange)="onSearch($event)" type="text"/>
+        <div class="googleList">
+            <div *ngFor="let item of items" class="googleListItem" (click)="setAddress(item.description, item.place_id)">
+                <b>{{ item.structured_formatting.main_text }}</b><br />
+                {{ item.structured_formatting.secondary_text }}
+            </div>
+        </div>
         <input type="text" class="street-address" id="address1" name="address1" [placeholder]="labels.address" autocomplete="shipping street-address address-line-1" [(ngModel)]="model.address1" (ngModelChange)="updateControl()"/>
         <input type="text" class="apt suite" id="address2" name="address2" [placeholder]="labels.apt" autocomplete="shipping address-line-2" [(ngModel)]="model.address2" (ngModelChange)="updateControl()"/>
         <input type="text" class="city locality" id="city" name="city" [placeholder]="labels.city" autocomplete="shipping city locality" [(ngModel)]="model.city" (ngModelChange)="updateControl()"/>
@@ -34,7 +42,10 @@ export class NovoAddressElement implements ControlValueAccessor, OnInit {
     onModelTouched: Function = () => {
     };
 
-    constructor(public labels: NovoLabelService) { }
+    searchTerm: any;
+    items: any[];
+
+    constructor(public labels: NovoLabelService, public googlePlacesService: GooglePlacesService) { }
 
     ngOnInit() {
         if (this.model) {
@@ -62,6 +73,54 @@ export class NovoAddressElement implements ControlValueAccessor, OnInit {
     onStateChange(evt) {
         this.model.state = evt;
         this.updateControl();
+    }
+
+    onSearch(event: any): void {
+        if (event) {
+            let _tempParams: any = {
+                query: event,
+                countryRestriction: '',
+                geoTypes: [],
+            };
+            this.googlePlacesService.getGeoPrediction(_tempParams).then((result: any) => {
+                this.items = result ? result : [];
+            });
+        } else {
+            this.items = [];
+        }
+    }
+
+    setAddress(address: string, placesId: string): void {
+        this.items = [];
+        this.googlePlacesService.getGeoPlaceDetail(placesId).then((details: any) => {
+        for (let i = 0; i < details.address_components.length; i++) {
+            let addressType: string = details.address_components[i].types[0];
+            switch (addressType) {
+                case 'street_number':
+                    this.model.address1 = details.address_components[i].long_name;
+                    break;
+                case 'route':
+                    this.model.address1 = this.model.address1 + ' ' + details.address_components[i].long_name;
+                    break;
+                case 'locality':
+                    this.model.city = details.address_components[i].long_name;
+                    break;
+                case 'administrative_area_level_1':
+                    this.model.state = details.address_components[i].short_name;
+                    break;
+                case 'country':
+                    this.model.countryName = details.address_components[i].long_name;
+                    // this.onCountryChange();
+                    break;
+                case 'postal_code':
+                    this.model.zip = details.address_components[i].short_name;
+                    break;
+                default:
+                break;
+            }
+            this.updateControl();
+        }
+        });
     }
 
     updateStates() {
