ede6007eaa15c521b7bdb9f9829e612b3f8913b4
Jenny Hoac <jhoac@bullhorn.com>
2018-03-08 17:44:23 -0500

    Readding matches clear in term so that it doesn't break custom picker code

--- src/platform/elements/picker/extras/base-picker-results/BasePickerResults.ts
+++ src/platform/elements/picker/extras/base-picker-results/BasePickerResults.ts
@@ -59,6 +59,7 @@ export class BasePickerResults {
     if (value !== this._term || this.page === 0) {
       this._term = value;
       this.page = 0;
+      this.matches = [];
       this.processSearch(true);
     }
   }
