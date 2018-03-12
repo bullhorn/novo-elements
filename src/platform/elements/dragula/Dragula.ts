cfffb089ad1d9798e0d89303bba48019cc7f6156
Brian Kimball <bvkimball@gmail.com>
2018-03-12 16:00:01 -0400

    feat(dragula): Using custom build with support for drag thresholds

--- src/platform/elements/dragula/Dragula.ts
+++ src/platform/elements/dragula/Dragula.ts
@@ -1,60 +1,60 @@
 // NG2
 import { Directive, ElementRef, OnInit, Input, OnChanges } from '@angular/core';
 // Vendor
-import * as dragulaImported from 'dragula';
+import * as dragulaImported from '@bullhorn/dragula';
 const dragula = dragulaImported;
 // APP
 import { NovoDragulaService } from './DragulaService';
 
 @Directive({
-    selector: '[dragula]'
+  selector: '[dragula]',
 })
 export class NovoDragulaElement implements OnInit, OnChanges {
-    @Input('dragula') bag: any;
-    @Input() dragulaModel: any;
-    drake: any = null;
-    container: any;
+  @Input('dragula') bag: any;
+  @Input() dragulaModel: any;
+  drake: any = null;
+  container: any;
 
-    constructor(element: ElementRef, private dragulaService: NovoDragulaService) {
-        this.container = element.nativeElement;
+  constructor(element: ElementRef, private dragulaService: NovoDragulaService) {
+    this.container = element.nativeElement;
+  }
+
+  ngOnInit() {
+    let bag = this.dragulaService.find(this.bag);
+
+    if (bag) {
+      this.drake = bag.drake;
+      this.checkModel();
+      this.drake.containers.push(this.container);
+    } else {
+      this.drake = dragula({
+        containers: [this.container],
+      });
+      this.checkModel();
+      this.dragulaService.add(this.bag, this.drake);
     }
+  }
 
-    ngOnInit() {
-        let bag = this.dragulaService.find(this.bag);
+  checkModel() {
+    if (this.dragulaModel) {
+      if (this.drake.models) {
+        this.drake.models.push(this.dragulaModel);
+      } else {
+        this.drake.models = [this.dragulaModel];
+      }
+    }
+  }
 
-        if (bag) {
-            this.drake = bag.drake;
-            this.checkModel();
-            this.drake.containers.push(this.container);
+  ngOnChanges(changes) {
+    if (changes && changes.dragulaModel) {
+      if (this.drake) {
+        if (this.drake.models) {
+          let modelIndex = this.drake.models.indexOf(changes.dragulaModel.previousValue);
+          this.drake.models.splice(modelIndex, 1, changes.dragulaModel.currentValue);
         } else {
-            this.drake = dragula({
-                containers: [this.container]
-            });
-            this.checkModel();
-            this.dragulaService.add(this.bag, this.drake);
+          this.drake.models = [changes.dragulaModel.currentValue];
         }
+      }
     }
-
-    checkModel() {
-        if (this.dragulaModel) {
-            if (this.drake.models) {
-                this.drake.models.push(this.dragulaModel);
-            } else {
-                this.drake.models = [this.dragulaModel];
-            }
-        }
-    }
-
-    ngOnChanges(changes) {
-        if (changes && changes.dragulaModel) {
-            if (this.drake) {
-                if (this.drake.models) {
-                    let modelIndex = this.drake.models.indexOf(changes.dragulaModel.previousValue);
-                    this.drake.models.splice(modelIndex, 1, changes.dragulaModel.currentValue);
-                } else {
-                    this.drake.models = [changes.dragulaModel.currentValue];
-                }
-            }
-        }
-    }
+  }
 }
