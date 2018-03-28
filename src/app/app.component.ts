dfa378c42295f8e83865512793c0d2d6167c4bef
Joshua Godi <joshuagodi@gmail.com>
2018-03-28 10:53:14 -0400

    chore(demo): Fiixng version link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,44 +5,42 @@ import { Router } from '@angular/router';
 import { NovoToastService, NovoModalService } from './../platform/index';
 
 @Component({
-    selector: 'demo-app',
-    templateUrl: './app.component.html',
+  selector: 'demo-app',
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-    menuOpen: boolean = false;
-    version: string;
-    designRoutes: Array<any>;
-    componentRoutes: Array<any>;
-    utilRoutes: Array<any>;
+  menuOpen: boolean = false;
+  designRoutes: Array<any>;
+  componentRoutes: Array<any>;
+  utilRoutes: Array<any>;
 
-    constructor(router: Router, private viewContainerRef: ViewContainerRef, toaster: NovoToastService, modalService: NovoModalService) {
-        toaster.parentViewContainer = viewContainerRef;
-        modalService.parentViewContainer = viewContainerRef;
+  constructor(router: Router, private viewContainerRef: ViewContainerRef, toaster: NovoToastService, modalService: NovoModalService) {
+    toaster.parentViewContainer = viewContainerRef;
+    modalService.parentViewContainer = viewContainerRef;
 
-        this.menuOpen = false;
-        this.version = 'VERSION';
+    this.menuOpen = false;
 
-        this.designRoutes = router.config.filter((r: any) => r.data.section === 'design').sort(this.sortMenu);
-        this.componentRoutes = router.config.filter((r: any) => r.data.section === 'components').sort(this.sortMenu);
-        this.utilRoutes = router.config.filter((r: any) => r.data.section === 'utils').sort(this.sortMenu);
+    this.designRoutes = router.config.filter((r: any) => r.data.section === 'design').sort(this.sortMenu);
+    this.componentRoutes = router.config.filter((r: any) => r.data.section === 'components').sort(this.sortMenu);
+    this.utilRoutes = router.config.filter((r: any) => r.data.section === 'utils').sort(this.sortMenu);
 
-        router.events.subscribe(() => {
-            window.scrollTo(0, 0);
-            this.menuOpen = false;
-        });
+    router.events.subscribe(() => {
+      window.scrollTo(0, 0);
+      this.menuOpen = false;
+    });
+  }
+
+  sortMenu(a, b) {
+    if (a.data.title < b.data.title) {
+      return -1;
     }
-
-    sortMenu(a, b) {
-        if (a.data.title < b.data.title) {
-            return -1;
-        }
-        if (a.data.title > b.data.title) {
-            return 1;
-        }
-        return 0;
+    if (a.data.title > b.data.title) {
+      return 1;
     }
+    return 0;
+  }
 
-    toggleMenu() {
-        this.menuOpen = !this.menuOpen;
-    }
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
