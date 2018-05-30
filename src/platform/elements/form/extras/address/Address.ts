349f4f8f0178cd2c0519a27088573d7cca548422
Tanvi Gadre <tgadre@bullhorn.com>
2018-05-30 16:47:58 -0400

    Revert "fix(address) updating model"
    This reverts commit 6e1500cd08727bc79fc54dcd6f77da799ac14691.

--- src/platform/elements/form/extras/address/Address.ts
+++ src/platform/elements/form/extras/address/Address.ts
@@ -192,7 +192,6 @@ export class NovoAddressElement implements ControlValueAccessor, OnInit {
       (field === 'state' &&
         this.config[field].required &&
         Helpers.isBlank(this.model.state) &&
-        !Helpers.isEmpty(this.model.state) &&
         !Helpers.isBlank(this.model.countryName) &&
         this.config[field].updated &&
         this.config.state.pickerConfig &&
