d017a885dcfd54641e60efb9ec98e1cafc3601cf
Chris Erxleben <christopher.erxleben@bullhorn.com>
2018-06-19 09:33:23 -0500

    (select): Updates to select accessibility

--- src/platform/elements/select/Select.module.ts
+++ src/platform/elements/select/Select.module.ts
@@ -1,16 +1,16 @@
-// NG2
+// NG
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { OverlayModule } from '@angular/cdk/overlay';
-// APP
+import { A11yModule } from '@angular/cdk/a11y';
+// App
 import { NovoOverlayModule } from '../overlay/Overlay.module';
 import { NovoSelectElement } from './Select';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, OverlayModule, NovoOverlayModule],
-    declarations: [NovoSelectElement],
-    exports: [NovoSelectElement]
+  imports: [CommonModule, FormsModule, OverlayModule, A11yModule, NovoOverlayModule],
+  declarations: [NovoSelectElement],
+  exports: [NovoSelectElement],
 })
-export class NovoSelectModule {
-}
+export class NovoSelectModule {}
