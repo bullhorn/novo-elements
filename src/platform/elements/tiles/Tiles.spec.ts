d8b7e9f9b05d463afc53ee87023552a041f7cdd0
Nathan Dickerson <ndickerson@bullhorn.com>
2018-07-24 14:55:55 -0500

    feat(tiles): Added event emitter for when selected tiles are clicked

--- src/platform/elements/tiles/Tiles.spec.ts
+++ src/platform/elements/tiles/Tiles.spec.ts
@@ -1,70 +1,118 @@
 // NG2
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 // APP
 import { NovoTilesElement } from './Tiles';
 
 describe('Elements: NovoTilesElement', () => {
-    let fixture;
-    let component;
+  let fixture;
+  let component;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [
-                NovoTilesElement
-            ]
-        }).compileComponents();
-        fixture = TestBed.createComponent(NovoTilesElement);
-        component = fixture.debugElement.componentInstance;
-    }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [NovoTilesElement],
+    }).compileComponents();
+    fixture = TestBed.createComponent(NovoTilesElement);
+    component = fixture.debugElement.componentInstance;
+    component.options = [
+      {
+        label: 'Red',
+        value: 'red',
+      },
+      {
+        label: 'Green',
+        value: 'green',
+      },
+      {
+        label: 'Blue',
+        value: 'blue',
+      },
+      {
+        label: 'Disabled',
+        value: 'disabled',
+        disabled: true,
+      },
+    ];
+    component.ngAfterContentInit();
+  }));
 
-    xdescribe('Method: select(event, item)', () => {
-        it('should be defined.', () => {
-            expect(component.select).toBeDefined();
-        });
-
-        it('should set label 2 with checked equal to true', () => {
-            component.options[1].checked = false;
-            component.select(false, component.options[1]);
-            expect(component.options[1].checked).toBeTruthy();
-        });
-
-        it('should only allow one tile to be checked true', () => {
-            component.ngOnInit();
-            component.select(false, component.options[0]);
-            expect(component.options[0].checked).toBeTruthy();
-            expect(component.options[1].checked).toBeFalsy();
-            expect(component.options[2].checked).toBeFalsy();
-            component.select(false, component.options[1]);
-            expect(component.options[0].checked).toBeFalsy();
-            expect(component.options[1].checked).toBeTruthy();
-            expect(component.options[2].checked).toBeFalsy();
-            component.select(false, component.options[2]);
-            expect(component.options[0].checked).toBeFalsy();
-            expect(component.options[1].checked).toBeFalsy();
-            expect(component.options[2].checked).toBeTruthy();
-        });
+  describe('Method: select(event, item)', () => {
+    it('should set label 2 with checked equal to true', () => {
+      let event: any = {
+        stopPropagation: () => {},
+        preventDefault: () => {},
+      };
+      expect(component.options[1].checked).toBeFalsy();
+      component.select(event, component.options[1]);
+      expect(component.options[1].checked).toBeTruthy();
     });
 
-    xdescribe('Method: writeValue()', () => {
-        it('should be defined.', () => {
-            expect(component.writeValue).toBeDefined();
-        });
-
-        it('should change the value', () => {
-            component.writeValue(10);
-            expect(component.model).toBe(10);
-        });
+    it('should only allow one tile to be checked true', () => {
+      component.select(false, component.options[0]);
+      expect(component.options[0].checked).toBeTruthy();
+      expect(component.options[1].checked).toBeFalsy();
+      expect(component.options[2].checked).toBeFalsy();
+      component.select(false, component.options[1]);
+      expect(component.options[0].checked).toBeFalsy();
+      expect(component.options[1].checked).toBeTruthy();
+      expect(component.options[2].checked).toBeFalsy();
+      component.select(false, component.options[2]);
+      expect(component.options[0].checked).toBeFalsy();
+      expect(component.options[1].checked).toBeFalsy();
+      expect(component.options[2].checked).toBeTruthy();
     });
 
-    describe('Method: registerOnChange()', () => {
-        it('should be defined.', () => {
-            expect(component.registerOnChange).toBeDefined();
-        });
+    it('should emit event but not allow disabled tiles to be checked', () => {
+      spyOn(component.onDisabledOptionClick, 'emit');
+      component.select(false, component.options[0]);
+      expect(component.options[0].checked).toBeTruthy();
+      expect(component.options[1].checked).toBeFalsy();
+      expect(component.options[2].checked).toBeFalsy();
+      expect(component.options[3].checked).toBeFalsy();
+      component.select(false, component.options[3]);
+      expect(component.options[0].checked).toBeTruthy();
+      expect(component.options[1].checked).toBeFalsy();
+      expect(component.options[2].checked).toBeFalsy();
+      expect(component.options[3].checked).toBeFalsy();
+      expect(component.onDisabledOptionClick.emit).toHaveBeenCalledTimes(1);
     });
 
-    describe('Method: registerOnTouched()', () => {
-        it('should be defined.', () => {
-            expect(component.registerOnTouched).toBeDefined();
-        });
+    it('should emit event when checked tiles are clicked', () => {
+      spyOn(component.onSelectedOptionClick, 'emit');
+      component.select(false, component.options[0]);
+      expect(component.options[0].checked).toBeTruthy();
+      expect(component.options[1].checked).toBeFalsy();
+      expect(component.options[2].checked).toBeFalsy();
+      expect(component.options[3].checked).toBeFalsy();
+      component.select(false, component.options[0]);
+      expect(component.options[0].checked).toBeTruthy();
+      expect(component.options[1].checked).toBeFalsy();
+      expect(component.options[2].checked).toBeFalsy();
+      expect(component.options[3].checked).toBeFalsy();
+      component.select(false, component.options[0]);
+      expect(component.options[0].checked).toBeTruthy();
+      expect(component.options[1].checked).toBeFalsy();
+      expect(component.options[2].checked).toBeFalsy();
+      expect(component.options[3].checked).toBeFalsy();
+      expect(component.onSelectedOptionClick.emit).toHaveBeenCalledTimes(2);
     });
+  });
+
+  describe('Method: writeValue()', () => {
+    it('should change the value', () => {
+      component.writeValue(10);
+      expect(component.model).toBe(10);
+    });
+  });
+
+  describe('Method: registerOnChange()', () => {
+    it('should be defined.', () => {
+      expect(component.registerOnChange).toBeDefined();
+    });
+  });
+
+  describe('Method: registerOnTouched()', () => {
+    it('should be defined.', () => {
+      expect(component.registerOnTouched).toBeDefined();
+    });
+  });
 });
