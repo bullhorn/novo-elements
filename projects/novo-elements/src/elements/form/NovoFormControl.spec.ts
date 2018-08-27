ab13c0181d8b793eb33e88e9f4bd531c1966647d
Joshua Godi <joshuagodi@gmail.com>
2018-08-27 11:43:21 -0700

    fix lint

--- projects/novo-elements/src/elements/form/NovoFormControl.spec.ts
+++ projects/novo-elements/src/elements/form/NovoFormControl.spec.ts
@@ -40,7 +40,7 @@ describe('Elements: NovoFormControl', () => {
       spyOn(component, 'setValidators').and.callThrough();
       spyOn(component, 'updateValueAndValidity').and.callThrough();
     });
-    it("should add new a validator and update the value and validity if it wasn't required before.", () => {
+    it('should add new a validator and update the value and validity if it wasn\'t required before.', () => {
       expect(component.setRequired).toBeDefined();
       component.hasRequiredValidator = false;
       component.setRequired(true);
@@ -50,7 +50,7 @@ describe('Elements: NovoFormControl', () => {
       expect(component.setValidators).toHaveBeenCalledWith([Validators.required]);
       expect(component.updateValueAndValidity).toHaveBeenCalled();
     });
-    it("should add new a validator and update the value and validity if it wasn't required before.", () => {
+    it('should add new a validator and update the value and validity if it wasn\'t required before.', () => {
       expect(component.setRequired).toBeDefined();
       component.hasRequiredValidator = true;
       component.setRequired(false);
