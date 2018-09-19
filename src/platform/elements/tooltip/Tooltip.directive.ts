3a03c2013d8a64b64bcaf0a5ecb0e7156f523858
Charles Barnes <charlesabarnesjr@gmail.com>
2018-09-19 00:15:57 -0500

    added initial positioning to the tooltip element

--- src/platform/elements/tooltip/Tooltip.directive.ts
+++ src/platform/elements/tooltip/Tooltip.directive.ts
@@ -1,7 +1,7 @@
 // NG2
-import { Directive, Input, HostListener, OnDestroy, ViewChild, Component, ViewContainerRef } from '@angular/core';
+import { Directive, Input, HostListener, OnDestroy, ViewChild, Component, ViewContainerRef, ElementRef } from '@angular/core';
 import { NovoOverlayTemplateComponent } from '../overlay/Overlay';
-import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { Overlay, OverlayRef, OverlayConfig, ConnectedPositionStrategy } from '@angular/cdk/overlay';
 import { NovoTooltip } from './Tooltip.component';
 import { ComponentPortal } from '@angular/cdk/portal';
 
@@ -32,8 +32,9 @@ export class TooltipDirective implements OnDestroy {
   private tooltipInstance: NovoTooltip | null;
   private portal: ComponentPortal<NovoTooltip>;
   private overlayRef: OverlayRef;
+  private scrollStrategy: string;
 
-  constructor(protected overlay: Overlay, private viewContainerRef: ViewContainerRef) {}
+  constructor(protected overlay: Overlay, private viewContainerRef: ViewContainerRef, private elementRef: ElementRef) {}
   isPosition(position: string): boolean {
     return position.toLowerCase() === (this.position || '').toLowerCase();
   }
@@ -63,10 +64,19 @@ export class TooltipDirective implements OnDestroy {
   }
 
   private show(): void {
-    this.overlayRef = this.overlay.create({
-      height: '400px',
-      width: '600px',
-    });
+    const overlayState = new OverlayConfig();
+
+    // Get the Connected position stategy (There are other positions strategies like in the middle of the screen for modals)
+    overlayState.positionStrategy = this.getPosition();
+
+    if (this.scrollStrategy === 'reposition') {
+      overlayState.scrollStrategy = this.overlay.scrollStrategies.reposition();
+    } else {
+      overlayState.scrollStrategy = this.overlay.scrollStrategies.close();
+    }
+    overlayState.scrollStrategy.enable();
+
+    this.overlayRef = this.overlay.create(overlayState);
 
     this.overlayRef.detach();
     this.portal = this.portal || new ComponentPortal(NovoTooltip, this.viewContainerRef);
@@ -74,5 +84,56 @@ export class TooltipDirective implements OnDestroy {
     let tooltipInstance = this.overlayRef.attach(this.portal).instance;
     tooltipInstance.message = this.tooltip;
     tooltipInstance!.show();
+  }
+
+  private getPosition(): ConnectedPositionStrategy {
+    // The tooltip will try to be placed in the position given.
+    // But it has a FallbackStrategy to avoid tooltips outside of screen and that type of issues
+    if (this.position === 'right') {
+      const strategy = this.overlay
+        .position()
+        .connectedTo(this.elementRef, { originX: 'end', originY: 'center' }, { overlayX: 'start', overlayY: 'center' })
+        .withOffsetX(20)
+        .withOffsetY(0);
+
+      return this.withFallbackStrategy(strategy);
+    } else if (this.position === 'bottom') {
+      const strategy = this.overlay
+        .position()
+        .connectedTo(this.elementRef, { originX: 'center', originY: 'bottom' }, { overlayX: 'center', overlayY: 'top' })
+        .withOffsetX(0)
+        .withOffsetY(20);
+
+      return this.withFallbackStrategy(strategy);
+    } else if (this.position === 'top') {
+      const strategy = this.overlay
+        .position()
+        .connectedTo(this.elementRef, { originX: 'center', originY: 'top' }, { overlayX: 'center', overlayY: 'bottom' })
+        .withOffsetX(0)
+        .withOffsetY(-20);
+
+      return this.withFallbackStrategy(strategy);
+    } else if (this.position === 'left') {
+      const strategy = this.overlay
+        .position()
+        .connectedTo(this.elementRef, { originX: 'start', originY: 'center' }, { overlayX: 'end', overlayY: 'center' })
+        .withOffsetX(-20)
+        .withOffsetY(0);
+
+      return this.withFallbackStrategy(strategy);
+    }
+  }
+  private withFallbackStrategy(strategy: ConnectedPositionStrategy): ConnectedPositionStrategy {
+    strategy
+      .withFallbackPosition({ originX: 'center', originY: 'bottom' }, { overlayX: 'center', overlayY: 'top' }, 0, 20)
+      .withFallbackPosition({ originX: 'end', originY: 'bottom' }, { overlayX: 'end', overlayY: 'top' }, 0, 20)
+      .withFallbackPosition({ originX: 'end', originY: 'center' }, { overlayX: 'start', overlayY: 'center' }, 20, 0)
+      .withFallbackPosition({ originX: 'start', originY: 'center' }, { overlayX: 'end', overlayY: 'center' }, -20, 0)
+      .withFallbackPosition({ originX: 'center', originY: 'top' }, { overlayX: 'center', overlayY: 'bottom' }, 0, -20)
+      .withFallbackPosition({ originX: 'start', originY: 'bottom' }, { overlayX: 'start', overlayY: 'top' }, 0, 20)
+      .withFallbackPosition({ originX: 'start', originY: 'top' }, { overlayX: 'start', overlayY: 'bottom' }, 0, -20)
+      .withFallbackPosition({ originX: 'end', originY: 'top' }, { overlayX: 'end', overlayY: 'bottom' }, 0, -20);
+
+    return strategy;
   }
 }
