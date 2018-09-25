2606f845ac9876398325a141a5c6c6495d3bfd93
Christopher Erxleben <cerxleben.fhs@gmail.com>
2018-09-25 15:25:33 -0500

    fix(picker): Change to handle empty use case (#822)

--- src/platform/elements/picker/extras/entity-picker-results/EntityPickerResults.ts
+++ src/platform/elements/picker/extras/entity-picker-results/EntityPickerResults.ts
@@ -105,9 +105,8 @@ export class EntityPickerResult {
    * @description This function should return a <strong>-tag wrapped HTML string.
    */
   highlight(match, query) {
-    query = query.trim();
     // Replaces the capture string with a the same string inside of a "strong" tag
-    return query && match ? match.replace(new RegExp(this.escapeRegexp(query), 'gi'), '<strong>$&</strong>') : match;
+    return query && match ? match.replace(new RegExp(this.escapeRegexp(query.trim()), 'gi'), '<strong>$&</strong>') : match;
   }
 
   getIconForResult(result?: any): string {
