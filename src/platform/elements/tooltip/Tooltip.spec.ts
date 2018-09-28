489314efdae2aa61968cfac48939b2bd915ad9e6
Nicholas Lewis <nlewis@bullhorn.com>
2018-09-28 13:22:03 -0400

    Fix up paths for unit tests

--- src/platform/elements/tooltip/Tooltip.spec.ts
+++ src/platform/elements/tooltip/Tooltip.spec.ts
@@ -2,30 +2,27 @@
 import { Component } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 // App
-import { TooltipDirective } from './Tooltip';
+import { TooltipDirective } from './Tooltip.directive';
 
 @Component({
-    selector: 'test-component',
-    template: `<div tooltip="test" tooltipPosition="right"></div>`
+  selector: 'test-component',
+  template: `<div tooltip="test" tooltipPosition="right"></div>`,
 })
 class TestComponent {}
 
 describe('Elements: TooltipDirective', () => {
-    let fixture;
-    let component;
+  let fixture;
+  let component;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [
-                TooltipDirective,
-                TestComponent
-            ]
-        }).compileComponents();
-        fixture = TestBed.createComponent(TestComponent);
-        component = fixture.debugElement.componentInstance;
-    }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TooltipDirective, TestComponent],
+    }).compileComponents();
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.debugElement.componentInstance;
+  }));
 
-    it('should initialize with defaults', () => {
-        expect(component).toBeDefined();
-    });
+  it('should initialize with defaults', () => {
+    expect(component).toBeDefined();
+  });
 });
