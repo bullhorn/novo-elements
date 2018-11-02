807459ba7818a73a4066fdaff3546520e97e50e7
jay-quach-tbx <43962425+jay-quach-tbx@users.noreply.github.com>
2018-11-02 15:26:18 -0400

    feat(value) added entityIconClass  (#838)

--- src/platform/elements/value/Value.ts
+++ src/platform/elements/value/Value.ts
@@ -16,27 +16,33 @@ export enum NOVO_VALUE_THEME {
 @Component({
   selector: 'novo-value',
   template: `
-        <ng-container [ngSwitch]="type">
-            <div class="value-outer" *ngIf="showLabel">
-                <label>{{ meta.label }}</label>
-                <a *ngSwitchCase="NOVO_VALUE_TYPE.INTERNAL_LINK" class="value" (click)="openLink()" [innerHTML]="data | render : meta"></a>
-                <a *ngSwitchCase="NOVO_VALUE_TYPE.LINK" class="value" [href]="url" target="_blank" [innerHTML]="data | render : meta"></a>
-                <novo-entity-list *ngSwitchCase="NOVO_VALUE_TYPE.ENTITY_LIST" [data]='data' [meta]="meta"></novo-entity-list>
-            </div>
-            <div *ngSwitchDefault class="value-outer" [ngClass]="customClass">
-                <label>{{ meta.label }}</label>
-                <div *ngIf="isDefault" class="value" [innerHTML]="data | render : meta"></div>
-            </div>
-            <div class="actions" *ngIf="showIcon">
-                <i *ngFor="let icon of meta.icons" [class]="iconClass(icon)" (click)="onValueClick(icon)"></i>
-            </div>
-        </ng-container>
-    `,
+    <ng-container [ngSwitch]="type">
+        <div class="value-outer" *ngIf="showLabel">
+            <label>{{ meta.label }}</label>
+            <span class="value">
+              <i *ngIf="meta.showEntityIcon" class="bhi-circle {{meta.entityIconClass}}"></i>
+              <a *ngSwitchCase="NOVO_VALUE_TYPE.INTERNAL_LINK" (click)="openLink()" [innerHTML]="data | render : meta"></a>
+              <a *ngSwitchCase="NOVO_VALUE_TYPE.LINK" class="value" [href]="url" target="_blank" [innerHTML]="data | render : meta"></a>
+            </span>
+            <novo-entity-list *ngSwitchCase="NOVO_VALUE_TYPE.ENTITY_LIST" [data]='data' [meta]="meta"></novo-entity-list>
+        </div>
+        <div *ngSwitchDefault class="value-outer" [ngClass]="customClass">
+            <label>{{ meta.label }}</label>
+            <div *ngIf="isDefault" class="value" [innerHTML]="data | render : meta"></div>
+        </div>
+        <div class="actions" *ngIf="showIcon">
+            <i *ngFor="let icon of meta.icons" [class]="iconClass(icon)" (click)="onValueClick(icon)"></i>
+        </div>
+    </ng-container>
+  `,
 })
 export class NovoValueElement implements OnInit, OnChanges {
-  @Input() data: any; // TODO use interface
-  @Input() meta: any; // TODO use interface
-  @Input() theme: NOVO_VALUE_THEME = NOVO_VALUE_THEME.DEFAULT;
+  @Input()
+  data: any; // TODO use interface
+  @Input()
+  meta: any; // TODO use interface
+  @Input()
+  theme: NOVO_VALUE_THEME = NOVO_VALUE_THEME.DEFAULT;
 
   type: NOVO_VALUE_TYPE;
   NOVO_VALUE_TYPE = NOVO_VALUE_TYPE;
