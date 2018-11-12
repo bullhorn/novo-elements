29fa39d796d5f3de7b3f7a7b59bf9a2f0510c892
ealltrell <elieel@gmail.com>
2018-11-12 18:38:30 -0500

    fix(Address): Update to control disabled state via country selection and readOnly binding
    Updated the novo-picker element to toggle disabled state through disabled.state flag
    Updated readOnly variable binding setter to call updateStates() to keep disabled.state current

--- projects/novo-elements/src/elements/form/extras/address/Address.ts
+++ projects/novo-elements/src/elements/form/extras/address/Address.ts
@@ -64,7 +64,7 @@ export interface NovoAddressConfig {
                 class="required-indicator"
                 [ngClass]="{'bhi-circle': !valid.state, 'bhi-check': valid.state}">
             </i>
-            <novo-picker [config]="config?.state?.pickerConfig" [placeholder]="config?.state?.label" (changed)="onStateChange($event)" autocomplete="shipping region" [(ngModel)]="model.state" [disablePickerInput]="disabled.state || !valid.countryID"></novo-picker>
+            <novo-picker [config]="config?.state?.pickerConfig" [placeholder]="config?.state?.label" (changed)="onStateChange($event)" autocomplete="shipping region" [(ngModel)]="model.state" [disablePickerInput]="disabled.state"></novo-picker>
         </span>
         <span *ngIf="!config?.zip?.hidden" class="zip postal-code" [class.invalid]="invalid.zip" [class.focus]="focused.zip" [class.disabled]="disabled.zip">
             <i *ngIf="config.zip.required"
@@ -90,8 +90,11 @@ export class NovoAddressElement implements ControlValueAccessor, OnInit {
   set readOnly(readOnly: boolean) {
     this._readOnly = readOnly;
     this.fieldList.forEach((field: string) => {
-      this.disabled[field] = this.readOnly;
+      this.disabled[field] = this._readOnly;
     });
+    if (this.model) {
+      this.updateStates();
+    }
   }
   get readOnly(): boolean {
     return this._readOnly;
