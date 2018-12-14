af1013ec22f78b71618361cc28a15d3883ebe563
Adrian Ossi <adrianossi@users.noreply.github.com>
2018-12-14 13:49:27 -0600

    Fix(form) - allow multiple flag on TO_MANY fields  (#877)

--- src/platform/elements/form/FormInterfaces.ts
+++ src/platform/elements/form/FormInterfaces.ts
@@ -1,21 +1,33 @@
 export interface NovoFormGroup {
-    layout?: any;
-    controls: any[];
-    novoControls: any[];
-    fieldsets: any[];
-    value: any;
-    valid?: boolean;
-    getRawValue?: any;
+  layout?: any;
+  controls: any[];
+  novoControls: any[];
+  fieldsets: any[];
+  value: any;
+  valid?: boolean;
+  getRawValue?: any;
 }
 
 export interface NovoFieldset {
-    title?: string;
-    icon?: string;
-    controls: any[];
+  title?: string;
+  icon?: string;
+  controls: any[];
 }
 
 export interface IFieldInteractionEvent {
-    controlKey: string;
-    prop: string;
-    value: any;
+  controlKey: string;
+  prop: string;
+  value: any;
+}
+
+export interface FormField {
+  dataSpecialization: string;
+  inputType: string;
+  options: string;
+  multiValue: boolean;
+  dataType: string;
+  type: string;
+  associatedEntity?: { entity: string };
+  optionsUrl?: string;
+  optionsType?: string;
 }
