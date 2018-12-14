f610b48cfb6ee82e872b52062bbc367950bddf7c
Sr. Travis <hi@travis.org>
2018-12-14 15:11:34 +0000

    Auto-generated commit -- fix(FormUtils): Respect Allow Multiple false for to-many association fields (#874)
    Update FormUtils to respect the "Allow Multiple" flag when it's configured to false for fields with to-many associations (by selecting type "picker"/"entitypicker" instead of "chips"/"entitychips").
    
    Triggered by commit: https://github.com/bullhorn/novo-elements/commit/40fd5d58a387d84dc52fd1326f388cc1bf89fc2f
    Travis CI build: https://travis-ci.org/bullhorn/novo-elements/builds/468035889

--- main.js
+++ main.js
@@ -18096,6 +18096,7 @@ var BaseControl = /** @class */ (function () {
         this.encrypted = !!config.encrypted;
         this.sortOrder = config.sortOrder === undefined ? 1 : config.sortOrder;
         this.controlType = config.controlType || '';
+        this.metaType = config.metaType;
         this.placeholder = config.placeholder || '';
         this.config = config.config || null;
         this.dirty = !!config.value;
@@ -18859,7 +18860,7 @@ var FormUtils = /** @class */ (function () {
             'Person',
             'Placement',
         ];
-        this.PICKER_TEST_LIST = [
+        this.PICKER_TEXT_LIST = [
             'CandidateText',
             'ClientText',
             'ClientContactText',
@@ -18940,6 +18941,23 @@ var FormUtils = /** @class */ (function () {
         return this.toFormGroup(controls);
     };
     /**
+     * @name hasAssociatedEntity
+     * @param field
+     */
+    /**
+     * \@name hasAssociatedEntity
+     * @param {?} field
+     * @return {?}
+     */
+    FormUtils.prototype.hasAssociatedEntity = /**
+     * \@name hasAssociatedEntity
+     * @param {?} field
+     * @return {?}
+     */
+    function (field) {
+        return !!(field.associatedEntity && ~this.ASSOCIATED_ENTITY_LIST.indexOf(field.associatedEntity.entity));
+    };
+    /**
      * @name determineInputType
      * @param field
      */
@@ -18997,15 +19015,25 @@ var FormUtils = /** @class */ (function () {
             Integer: 'number',
         };
         if (field.type === 'TO_MANY') {
-            if (field.associatedEntity && ~this.ASSOCIATED_ENTITY_LIST.indexOf(field.associatedEntity.entity)) {
-                type = 'entitychips'; // TODO!
+            if (this.hasAssociatedEntity(field)) {
+                if (field.multiValue === false) {
+                    type = 'entitypicker';
+                }
+                else {
+                    type = 'entitychips';
+                }
             }
             else {
-                type = 'chips';
+                if (field.multiValue === false) {
+                    type = 'picker';
+                }
+                else {
+                    type = 'chips';
+                }
             }
         }
         else if (field.type === 'TO_ONE') {
-            if (field.associatedEntity && ~this.ASSOCIATED_ENTITY_LIST.indexOf(field.associatedEntity.entity)) {
+            if (this.hasAssociatedEntity(field)) {
                 type = 'entitypicker'; // TODO!
             }
             else {
@@ -19013,7 +19041,7 @@ var FormUtils = /** @class */ (function () {
             }
         }
         else if (field.optionsUrl && field.inputType === 'SELECT') {
-            if (field.optionsType && ~this.PICKER_TEST_LIST.indexOf(field.optionsType)) {
+            if (field.optionsType && ~this.PICKER_TEXT_LIST.indexOf(field.optionsType)) {
                 type = 'entitypicker'; // TODO!
             }
             else {
@@ -19083,6 +19111,7 @@ var FormUtils = /** @class */ (function () {
         var control;
         /** @type {?} */
         var controlConfig = {
+            metaType: field.type,
             type: type,
             key: field.name,
             label: field.label,
