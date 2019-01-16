b8dbc6fe89f90c823dfa75d81ec904e3d05e3840
AJ Wu <awu@bullhorn.com>
2019-01-16 10:48:12 -0800

    fix(state) - allowing state to have optionType other than State (#891)

--- projects/novo-elements/src/utils/form-utils/FormUtils.ts
+++ projects/novo-elements/src/utils/form-utils/FormUtils.ts
@@ -388,9 +388,7 @@ export class FormUtils {
               controlConfig.value[subfield.name] = 1;
             }
             if (subfield.name === 'state' || subfield.name === 'countryID') {
-              if (subfield.name === 'state') {
-                subfield.optionsType = 'State';
-              } else if (subfield.name === 'countryID') {
+              if (subfield.name === 'countryID') {
                 subfield.optionsType = 'Country';
               }
               if (!subfield.optionsUrl) {
