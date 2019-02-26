d0f841967462c95aa19155a2b0baa813a3c95feb
Chris Erxleben <christopher.erxleben@bullhorn.com>
2019-02-26 11:42:25 -0600

    feat(helpers): Extended the logic of isNumber

--- projects/novo-elements/src/utils/Helpers.ts
+++ projects/novo-elements/src/utils/Helpers.ts
@@ -83,8 +83,14 @@ export class Helpers {
     return typeof obj === 'string';
   }
 
-  static isNumber(obj: any) {
-    return obj && !isNaN(parseInt(obj, 10));
+  static isNumber(val: any) {
+    if (typeof val === 'number') {
+      return !isNaN(val);
+    } else if (typeof val === 'string') {
+      return val.length > 0 && val !== '.' && /^\d*\.?\d*$/.test(val);
+    } else {
+      return val && !isNaN(parseInt(val, 10));
+    }
   }
 
   /**
