f31a41ae494422fbc2ec522f066b82704c0eb5e9
Sr. Travis <hi@travis.org>
2019-03-28 21:52:51 +0000

    Auto-generated commit -- feat(value) Making associated Leads type of INTERNAL_LINK (#934)
    Triggered by commit: https://github.com/bullhorn/novo-elements/commit/5c1d351b30d7540e1d8a1792dbbd6a7785751380
    Travis CI build: https://travis-ci.org/bullhorn/novo-elements/builds/512721655

--- main.js
+++ main.js
@@ -45071,6 +45071,7 @@ var NovoValueElement = /** @class */ (function () {
                 case 'Opportunity':
                 case 'JobOrder':
                 case 'Placement':
+                case 'Lead':
                     this._type = NOVO_VALUE_TYPE.INTERNAL_LINK;
                     break;
                 default:
