d3aaadfca560b396310ddfe4a51dcd24932f02d6
Kevin Frederiksen <kevin.frederiksen@bullhorn.com>
2019-07-02 09:59:02 -0500

    BH-61571: Add Unit test

--- projects/novo-elements/src/elements/form/Control.spec.ts
+++ projects/novo-elements/src/elements/form/Control.spec.ts
@@ -1,10 +1,21 @@
 // NG2
-import { Component, ElementRef } from '@angular/core';
-import { TestBed, async } from '@angular/core/testing';
+import {ChangeDetectorRef, Component, ElementRef, NO_ERRORS_SCHEMA} from '@angular/core';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 // App
 import { NovoAutoSize } from './Control';
 import { NovoControlElement } from './Control';
+import {NovoFormControl} from "./NovoFormControl";
+import {EntityPickerResult, FieldInteractionApi, NovoLabelService, NovoListElement, NovoTemplateService} from "../..";
+import {NovoLoadingElement} from "../loading/Loading";
+import {
+  NovoItemAvatarElement,
+  NovoItemContentElement,
+  NovoItemHeaderElement,
+  NovoItemTitleElement,
+  NovoListItemElement
+} from "../list/List";
+import {DateFormatService} from "../../services/date-format/DateFormat";
 
 @Component({
   selector: 'novo-auto-size-test-component',
@@ -70,5 +81,79 @@ describe('Test Localization', () => {
   it('should set decimal separator based on locale correctly', () => {
     let component = new NovoControlElement(mockElement, null, null, null, null, null, 'fr-FR');
     expect(component.decimalSeparator).toBe('.');
+  });
+});
+
+@Component({
+  template: `
+    <div></div>
+  `
+})
+class TestComponent {}
+describe("NovoControlElement", () => {
+  let component: NovoControlElement;
+  let fixture: ComponentFixture<NovoControlElement>;
+  beforeEach(() => {
+    const elementRefStub = {
+      nativeElement: { style: { height: {}, minHeight: {} }, scrollHeight: {} }
+    };
+    const changeDetectorRefStub = { markForCheck: () => ({}) };
+    const novoLabelServiceStub = { invalidIntegerInput: {} };
+    const dateFormatServiceStub = {};
+    const fieldInteractionApiStub = { form: {}, currentKey: {} };
+    const novoTemplateServiceStub = { getAll: () => ({}) };
+    TestBed.configureTestingModule({
+      declarations: [NovoAutoSize, TestComponent, NovoControlElement],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        NovoAutoSize,
+        { provide: ElementRef, useValue: elementRefStub },
+        { provide: ChangeDetectorRef, useValue: changeDetectorRefStub },
+        { provide: NovoLabelService, useValue: novoLabelServiceStub },
+        { provide: DateFormatService, useValue: dateFormatServiceStub },
+        { provide: FieldInteractionApi, useValue: fieldInteractionApiStub },
+        { provide: NovoTemplateService, useValue: novoTemplateServiceStub }
+      ]
+    });
+    fixture = TestBed.createComponent(NovoControlElement);
+    component = fixture.componentInstance;
+  });
+
+  it('should return false if the field has a MAX_LENGTH property but is not focused', () => {
+    // Arrange
+    component.control = {
+       key: 0,
+     };
+    component.form = {
+      controls: [{
+        maxLength: 1,
+      }],
+    };
+
+    // Act
+    let testBoolean = component.showCount;
+
+    // Assert
+    expect(testBoolean).toEqual(false);
+  });
+
+  it('should return true if the field has a MAX_LENGTH property but is not focused', () => {
+    // Arrange
+    component.control = {
+      key: 0,
+    };
+    component.form = {
+      controls: [{
+        maxLength: 1,
+      }],
+    };
+
+    (component as any)._focused = true;
+
+    // Act
+    let testBoolean = component.showCount;
+
+    // Assert
+    expect(testBoolean).toEqual(true);
   });
 });
