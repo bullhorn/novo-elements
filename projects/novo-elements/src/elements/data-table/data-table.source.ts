ceb2964ab9874921865813c4893c9c2fa269faed
Michael Dill <mdill@bullhorn.com>
2019-07-02 08:42:05 -0500

    unsubscribe

--- projects/novo-elements/src/elements/data-table/data-table.source.ts
+++ projects/novo-elements/src/elements/data-table/data-table.source.ts
@@ -1,18 +1,19 @@
-import { ChangeDetectorRef } from '@angular/core';
+import {ChangeDetectorRef, OnDestroy} from '@angular/core';
 import { DataSource } from '@angular/cdk/table';
-import { Observable, merge, of } from 'rxjs';
+import {Observable, merge, of, Subscription} from 'rxjs';
 import { startWith, switchMap, map, catchError } from 'rxjs/operators';
 
 import { DataTableState } from './state/data-table-state.service';
 import { IDataTableService } from './interfaces';
 
-export class DataTableSource<T> extends DataSource<T> {
+export class DataTableSource<T> extends DataSource<T> implements OnDestroy {
   public total = 0;
   public currentTotal = 0;
   public current = 0;
   public loading = false;
   public pristine = true;
   public data: T[];
+  private connectSub: Subscription;
 
   private totalSet: boolean = false;
 
@@ -26,7 +27,7 @@ export class DataTableSource<T> extends DataSource<T> {
 
   constructor(private tableService: IDataTableService<T>, private state: DataTableState<T>, private ref: ChangeDetectorRef) {
     super();
-    this.connect().subscribe(() => {
+    this.connectSub = this.connect().subscribe(() => {
       if (!this.totalSet || this.currentTotal > this.total) {
         this.total = this.currentTotal;
         this.totalSet = true;
@@ -34,6 +35,10 @@ export class DataTableSource<T> extends DataSource<T> {
       this.loading = false;
       this.ref.markForCheck();
     });
+  }
+
+  ngOnDestroy(): void {
+    this.connectSub.unsubscribe();
   }
 
   public connect(): Observable<any> {
