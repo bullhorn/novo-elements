02eac00ae5a44b83e7dbd870139cefe50dd20b13
ezra.shim@bullhorn.com <ezra.shim@bullhorn.com>
2019-07-05 15:37:57 -0400

    delete novo-chip-container

--- projects/novo-elements/src/elements/chips/Chips.ts
+++ projects/novo-elements/src/elements/chips/Chips.ts
@@ -21,12 +21,11 @@ const CHIPS_VALUE_ACCESSOR = {
 @Component({
   selector: 'chip,novo-chip',
   template: `
-        <span (click)="onSelect($event)" (mouseenter)="onSelect($event)" (mouseleave)="onDeselect($event)" [ngClass]="_type">
-            <i *ngIf="_type" class="bhi-circle"></i>
-            <span><ng-content></ng-content></span>
-        </span>
-        <i class="bhi-close" *ngIf="!disabled" (click)="onRemove($event)"></i>
-    `,
+    <span (click)="onSelect($event)" (mouseenter)="onSelect($event)" (mouseleave)="onDeselect($event)" [ngClass]="_type">
+      <i *ngIf="_type" class="bhi-circle"></i> <span><ng-content></ng-content></span>
+    </span>
+    <i class="bhi-close" *ngIf="!disabled" (click)="onRemove($event)"></i>
+  `,
 })
 export class NovoChipElement {
   @Input()
@@ -79,41 +78,41 @@ export class NovoChipElement {
   selector: 'chips,novo-chips',
   providers: [CHIPS_VALUE_ACCESSOR],
   template: `
-        <div class="novo-chip-container">
-          <novo-chip
-              *ngFor="let item of _items | async"
-              [type]="type || item?.value?.searchEntity"
-              [class.selected]="item == selected"
-              [disabled]="disablePickerInput"
-              (remove)="remove($event, item)"
-              (select)="select($event, item)"
-              (deselect)="deselect($event, item)">
-              {{ item.label }}
-          </novo-chip>
-        </div>
-        <div class="chip-input-container" *ngIf="!maxlength || (maxlength && items.length < maxlength)">
-            <novo-picker
-                clearValueOnSelect="true"
-                [closeOnSelect]="closeOnSelect"
-                [config]="source"
-                [disablePickerInput]="disablePickerInput"
-                [placeholder]="placeholder"
-                [(ngModel)]="itemToAdd"
-                (select)="add($event)"
-                (keydown)="onKeyDown($event)"
-                (focus)="onFocus($event)"
-                (typing)="onTyping($event)"
-                (blur)="onTouched($event)"
-                [selected]="items"
-                [overrideElement]="element">
-            </novo-picker>
-        </div>
-        <div class="preview-container">
-            <span #preview></span>
-        </div>
-        <i class="bhi-search" [class.has-value]="items.length" *ngIf="!disablePickerInput"></i>
-        <label class="clear-all" *ngIf="items.length && !disablePickerInput" (click)="clearValue()">{{ labels.clearAll }} <i class="bhi-times"></i></label>
-   `,
+    <novo-chip
+      *ngFor="let item of (_items | async)"
+      [type]="type || item?.value?.searchEntity"
+      [class.selected]="item == selected"
+      [disabled]="disablePickerInput"
+      (remove)="remove($event, item)"
+      (select)="select($event, item)"
+      (deselect)="deselect($event, item)"
+    >
+      {{ item.label }}
+    </novo-chip>
+    <div class="chip-input-container" *ngIf="!maxlength || (maxlength && items.length < maxlength)">
+      <novo-picker
+        clearValueOnSelect="true"
+        [closeOnSelect]="closeOnSelect"
+        [config]="source"
+        [disablePickerInput]="disablePickerInput"
+        [placeholder]="placeholder"
+        [(ngModel)]="itemToAdd"
+        (select)="add($event)"
+        (keydown)="onKeyDown($event)"
+        (focus)="onFocus($event)"
+        (typing)="onTyping($event)"
+        (blur)="onTouched($event)"
+        [selected]="items"
+        [overrideElement]="element"
+      >
+      </novo-picker>
+    </div>
+    <div class="preview-container"><span #preview></span></div>
+    <i class="bhi-search" [class.has-value]="items.length" *ngIf="!disablePickerInput"></i>
+    <label class="clear-all" *ngIf="items.length && !disablePickerInput" (click)="clearValue()"
+      >{{ labels.clearAll }} <i class="bhi-times"></i
+    ></label>
+  `,
   host: {
     '[class.with-value]': 'items.length > 0',
     '[class.disabled]': 'disablePickerInput',
