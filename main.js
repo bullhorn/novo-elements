5e8cb95e3ea479d7ee4b80157645cea34aba6e2d
Sr. Travis <hi@travis.org>
2019-07-12 15:20:54 +0000

    Auto-generated commit -- chore(docs): Pointing to the extension samples repo
    Triggered by commit: https://github.com/bullhorn/novo-elements/commit/d59d43a0ec88ebe63e9fd2cffd6eda4bf8c8641a
    Travis CI build: https://travis-ci.org/bullhorn/novo-elements/builds/557895937

--- main.js
+++ main.js
@@ -73825,7 +73825,7 @@ var PatternsPage = /** @class */ (function () {
     PatternsPage.decorators = [
         { type: _angular_core__WEBPACK_IMPORTED_MODULE_11__["Component"], args: [{
                     selector: 'patterns-page',
-                    template: "<h1>Design Patterns</h1><p>The following are examples of know composition patterns to create a common user exeperience.  The component library is design to be used in these patterns.</p><h2>Activity Sections</h2><p>Displaying a series of list data in ... blah, blah, blah</p><p><code-example example=\"activity-section\"></code-example></p><h2>Record Headers</h2><p>Entity Record Headers Patterns.</p><p><code-example example=\"record-header\"></code-example></p>"
+                    template: "<h1>Design Patterns</h1><p>The following are examples of know composition patterns to create a common user experience.  The component library is design to be used in these patterns.</p><h2>Activity Sections</h2><p>Displaying a series of lists in expandable sections, like in the activity tab.</p><p><code-example example=\"activity-section\"></code-example></p><h2>Record Headers</h2><p>Entity Record Headers Patterns.</p><p><code-example example=\"record-header\"></code-example></p>"
                 }] }
     ];
     return PatternsPage;
@@ -73836,7 +73836,7 @@ var TemplatesPage = /** @class */ (function () {
     TemplatesPage.decorators = [
         { type: _angular_core__WEBPACK_IMPORTED_MODULE_11__["Component"], args: [{
                     selector: 'templates-page',
-                    template: "<h2>Bullhorn Application Templates</h2><h2>Example starter templates for your application</h2><h3>Email</h3><p>List App</p><p>TODO: Add linked image</p><h3>Form App</h3><p>form-based app template</p><p>TODO: Add linked image</p><h3>Custom Card Starter Template</h3><p>empty starter template</p><p>TODO: Add linked image</p><h3>Dashboard</h3><p>dashboard template</p><p>TODO: Add linked image</p>"
+                    template: "<h2>Bullhorn Application Templates</h2><p>Bullhorn provides customization of the user experience with custom tabs, custom cards, custom actions and more.</p><p>The <a href=\"https://github.com/bullhorn/extension-samples\">Bullhorn Extension Samples</a> repoprovides several examples for getting started building custom apps using Novo Elements.</p>"
                 }] }
     ];
     return TemplatesPage;
