8483380537f801e056a3920f35923a84ee735be4
Michael Dill <Mddesignstl@gmail.com>
2019-07-18 12:04:03 -0500

    fix(data-table) Revert "Fix issue where novo data tables weren't showing results if they start filtered (#991)" (#1008)
    This reverts commit 5a25feaa610d0f48f9fabddcbe13adb4a8ac5b95.

--- projects/novo-elements/src/elements/data-table/data-table.source.ts
+++ projects/novo-elements/src/elements/data-table/data-table.source.ts
@@ -1,19 +1,18 @@
-import {ChangeDetectorRef, OnDestroy} from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
 import { DataSource } from '@angular/cdk/table';
-import {Observable, merge, of, Subscription} from 'rxjs';
+import { Observable, merge, of } from 'rxjs';
 import { startWith, switchMap, map, catchError } from 'rxjs/operators';
 
 import { DataTableState } from './state/data-table-state.service';
 import { IDataTableService } from './interfaces';
 
-export class DataTableSource<T> extends DataSource<T> implements OnDestroy {
+export class DataTableSource<T> extends DataSource<T> {
   public total = 0;
   public currentTotal = 0;
   public current = 0;
   public loading = false;
   public pristine = true;
   public data: T[];
-  private connectSub: Subscription;
 
   private totalSet: boolean = false;
 
@@ -27,18 +26,6 @@ export class DataTableSource<T> extends DataSource<T> implements OnDestroy {
 
   constructor(private tableService: IDataTableService<T>, private state: DataTableState<T>, private ref: ChangeDetectorRef) {
     super();
-    this.connectSub = this.connect().subscribe(() => {
-      if (!this.totalSet || this.currentTotal > this.total) {
-        this.total = this.currentTotal;
-        this.totalSet = true;
-      }
-      this.loading = false;
-      this.ref.markForCheck();
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.connectSub.unsubscribe();
   }
 
   public connect(): Observable<any> {
@@ -47,9 +34,7 @@ export class DataTableSource<T> extends DataSource<T> implements OnDestroy {
       startWith(null),
       switchMap(() => {
         this.pristine = false;
-          if (this.state.isForceRefresh || this.total === 0) {
-            this.loading = true;
-          }
+        this.loading = true;
         return this.tableService.getTableResults(
           this.state.sort,
           this.state.filter,
@@ -60,8 +45,9 @@ export class DataTableSource<T> extends DataSource<T> implements OnDestroy {
         );
       }),
       map((data: { results: T[]; total: number }) => {
-        if (this.state.isForceRefresh) {
-          this.totalSet = false;
+        if (!this.totalSet || this.state.isForceRefresh) {
+          this.total = data.total;
+          this.totalSet = true;
           this.state.isForceRefresh = false;
         }
         this.currentTotal = data.total;
@@ -74,13 +60,16 @@ export class DataTableSource<T> extends DataSource<T> implements OnDestroy {
         setTimeout(() => {
           this.ref.markForCheck();
           setTimeout(() => {
+            this.loading = false;
             this.state.dataLoaded.next();
+            this.ref.markForCheck();
           });
         });
         return data.results;
       }),
       catchError((err, caught) => {
         console.error(err, caught); // tslint: disable-line
+        this.loading = false;
         return of(null);
       }),
     );
