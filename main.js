90c0d24a367d3eca4e1706a55e4cae761459e8d8
Sr. Travis <hi@travis.org>
2019-07-18 17:14:05 +0000

    Auto-generated commit -- fix(data-table) Revert "Fix issue where novo data tables weren't showing results if they start filtered (#991)" (#1008)
    This reverts commit 5a25feaa610d0f48f9fabddcbe13adb4a8ac5b95.
    
    Triggered by commit: https://github.com/bullhorn/novo-elements/commit/8483380537f801e056a3920f35923a84ee735be4
    Travis CI build: https://travis-ci.org/bullhorn/novo-elements/builds/560587512

--- main.js
+++ main.js
@@ -49021,14 +49021,6 @@ DataTableSource = /** @class */ (function (_super) {
         _this.loading = false;
         _this.pristine = true;
         _this.totalSet = false;
-        _this.connectSub = _this.connect().subscribe(function () {
-            if (!_this.totalSet || _this.currentTotal > _this.total) {
-                _this.total = _this.currentTotal;
-                _this.totalSet = true;
-            }
-            _this.loading = false;
-            _this.ref.markForCheck();
-        });
         return _this;
     }
     Object.defineProperty(DataTableSource.prototype, "totallyEmpty", {
@@ -49054,15 +49046,6 @@ DataTableSource = /** @class */ (function (_super) {
     /**
      * @return {?}
      */
-    DataTableSource.prototype.ngOnDestroy = /**
-     * @return {?}
-     */
-    function () {
-        this.connectSub.unsubscribe();
-    };
-    /**
-     * @return {?}
-     */
     DataTableSource.prototype.connect = /**
      * @return {?}
      */
@@ -49072,13 +49055,12 @@ DataTableSource = /** @class */ (function (_super) {
         var displayDataChanges = [this.state.updates];
         return rxjs__WEBPACK_IMPORTED_MODULE_18__["merge"].apply(void 0, Object(tslib__WEBPACK_IMPORTED_MODULE_25__["__spread"])(displayDataChanges)).pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_19__["startWith"])(null), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_19__["switchMap"])(function () {
             _this.pristine = false;
-            if (_this.state.isForceRefresh || _this.total === 0) {
-                _this.loading = true;
-            }
+            _this.loading = true;
             return _this.tableService.getTableResults(_this.state.sort, _this.state.filter, _this.state.page, _this.state.pageSize, _this.state.globalSearch, _this.state.outsideFilter);
         }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_19__["map"])(function (data) {
-            if (_this.state.isForceRefresh) {
-                _this.totalSet = false;
+            if (!_this.totalSet || _this.state.isForceRefresh) {
+                _this.total = data.total;
+                _this.totalSet = true;
                 _this.state.isForceRefresh = false;
             }
             _this.currentTotal = data.total;
@@ -49091,12 +49073,15 @@ DataTableSource = /** @class */ (function (_super) {
             setTimeout(function () {
                 _this.ref.markForCheck();
                 setTimeout(function () {
+                    _this.loading = false;
                     _this.state.dataLoaded.next();
+                    _this.ref.markForCheck();
                 });
             });
             return data.results;
         }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_19__["catchError"])(function (err, caught) {
             console.error(err, caught); // tslint: disable-line
+            _this.loading = false;
             return Object(rxjs__WEBPACK_IMPORTED_MODULE_18__["of"])(null);
         }));
     };
