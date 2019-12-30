62ed838305f9c07ca30688e5e70ba68ba4d262db
Kevin Frederiksen <kevin.frederiksen@bullhorn.com>
2019-12-30 14:20:18 -0600

    BH-64754: Display time and date on novo-data-tables

--- projects/novo-elements/src/elements/data-table/data-table.pipes.ts
+++ projects/novo-elements/src/elements/data-table/data-table.pipes.ts
@@ -46,7 +46,8 @@ export class DateTableDateTimeRendererPipe<T> implements PipeTransform {
   constructor(private labels: NovoLabelService) {}
   transform(value: any, column: IDataTableColumn<T>): string {
     if (!Helpers.isEmpty(value)) {
-      return column.format ? value : this.labels.formatDate(interpolateCell<T>(value, column));
+      let val = interpolateCell<T>(value, column);
+      return this.labels.formatDateShort(val);
     }
     return '';
   }
