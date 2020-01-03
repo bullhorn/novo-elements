886773a533ec93618ed0e8a3c6d41771606ba0d0
kvnfrederiksen <32199701+kvnfrederiksen@users.noreply.github.com>
2020-01-03 13:39:02 -0600

    feat(data-tables) update date formatting (#1063)

---
+++ projects/novo-elements/src/elements/data-table/data-table.pipes.spec.ts
@@ -0,0 +1,47 @@
+// APP
+import {DateTableDateTimeRendererPipe} from './data-table.pipes';
+
+class MockNovoLabelService {
+  formatDateShort(val) {}
+}
+
+describe('Pipe: DateTableDateTimeRendererPipe', () => {
+  let labels;
+  let pipe;
+
+  beforeEach(() => {
+    labels = new MockNovoLabelService();
+    pipe = new DateTableDateTimeRendererPipe(labels);
+  });
+
+  describe('When rendering strings', () => {
+    it('should make a call to the novo-label service if the value is not null', () => {
+      // Arrange
+      spyOn(pipe.labels, 'formatDateShort');
+      const testVal = 1234567890;
+      const testColumn = {
+        label: 'Test',
+        id: 'test',
+        type: 'datetime',
+      };
+      // Act
+      pipe.transform(testVal, testColumn);
+      // Assert
+      expect(pipe.labels.formatDateShort).toHaveBeenCalled();
+    });
+    it('should return an empty string if the value is null', () => {
+      // Arrange
+      spyOn(pipe.labels, 'formatDateShort');
+      const testVal = null;
+      const testColumn = {
+        label: 'Test',
+        id: 'test',
+        type: 'datetime',
+      };
+      // Act
+      const testString = pipe.transform(testVal, testColumn);
+      // Assert
+      expect(testString).toEqual('');
+    });
+  });
+});
