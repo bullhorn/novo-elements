6b71199b0d16372dba1d9b4752763a5d47e76518
Jenny Hoac <jhoac@bullhorn.com>
2020-06-05 19:37:27 -0400

    Adding search bar to column config dropdown with some styles

--- projects/novo-elements/src/elements/data-table/configure-columns/configure-columns-dropdown.component.ts
+++ projects/novo-elements/src/elements/data-table/configure-columns/configure-columns-dropdown.component.ts
@@ -1,26 +1,36 @@
+// NG2
 import {
     ChangeDetectorRef,
     Component,
+    ElementRef,
     EventEmitter,
     Input,
-    OnInit,
+    OnChanges,
     Output,
 } from '@angular/core';
+// APP
+import { BasePickerResults } from '../../picker/extras/base-picker-results/BasePickerResults';
 
 @Component({
     selector: 'configure-columns-dropdown',
     templateUrl: './configure-columns-dropdown.html',
 })
-export class ConfigureColumnsDropdown implements OnInit {
+export class ConfigureColumnsDropdown extends BasePickerResults implements OnChanges {
     @Input() columns: any;
 
     public displayedColumns;
 
     @Output() applyColumnConfig: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-    constructor(private ref: ChangeDetectorRef) {}
+    constructor(
+        element: ElementRef,
+        ref: ChangeDetectorRef,
+    ) {
+        super(element, ref);
+    }
 
-    ngOnInit() {
+    ngOnChanges() {
+        this.matches = this.columns;
     }
 
     saveColumns(emit = true): void {
@@ -32,5 +42,15 @@ export class ConfigureColumnsDropdown implements OnInit {
                 this.applyColumnConfig.emit(this.displayedColumns);
             }
         }
+        this.clearQuickSearch();
+    }
+
+    clearQuickSearch(): void {
+        this.term = '';
+    }
+    
+    processSearch(): void {
+        this.matches = this.columns.filter((column) => column.label.toLowerCase().includes(this.term.toLowerCase()));
+        this.ref.markForCheck();
     }
 }
