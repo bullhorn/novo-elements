5dbf0056eb4d22c89fac7b23687375d1b846c55c
Thibaut Lassalle <thibaut.lassalle@bullhorn.com>
2020-07-15 11:16:32 -0400

    fix unit test

---
+++ projects/novo-elements/src/elements/unless/TestCmp.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'test-cmp',
+    template: `
+          <div bhUnless="false"></div>
+      `,
+})
+export class TestCmp { }
