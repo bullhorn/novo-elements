5724ce3e0dea3fda3618b231e90dcdc6cb2eef35
Thibaut Lassalle <thibaut.lassalle@bullhorn.com>
2020-07-22 08:01:12 -0400

    chore(libs) update

--- demo/polyfills.ts
+++ demo/polyfills.ts
@@ -21,7 +21,7 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js'; // Run `npm install --save classlist.js`.
 
-import 'core-js/es/array';
+// import 'core-js/es/array';
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
