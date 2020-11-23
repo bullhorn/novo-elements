5e105a8eb6314d3f419d8107fc806d5db4ae79c4
Michael Dill <mdill@bullhorn.com>
2020-11-23 10:14:32 -0600

    feat(NovoDataTable): Changes for list interactions to be enabled (#1139)
    * Changes for list interactions to be enabled
    
    * safety check for list interactions
    
    * better typing

---
+++ projects/novo-elements/src/elements/data-table/ListInteractionTypes.ts
@@ -0,0 +1,11 @@
+import { NovoDataTable } from './data-table.component';
+
+export type ListInteractionEvent = 'init' | 'change';
+
+export type ListInteraction = {
+  script: (novoDataTable: NovoDataTable<any>, columnId: string) => void;
+  action: string;
+  event: ListInteractionEvent[];
+};
+
+export type ListInteractionDictionary = { [key: string]: ListInteraction[] };
