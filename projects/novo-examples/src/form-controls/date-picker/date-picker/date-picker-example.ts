c67cba4f30de48e236bd0918facba8e80252c6d7
JorgeBerroa <jorge.berroa@bullhorn.com>
2021-02-18 13:02:20 -0500

    fix linting issues

--- projects/novo-examples/src/form-controls/date-picker/date-picker/date-picker-example.ts
+++ projects/novo-examples/src/form-controls/date-picker/date-picker/date-picker-example.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NovoLabelService } from 'novo-elements';
 
 
-//  Must add 'ExtendedLabelService' to your module file and  
+//  Must add 'ExtendedLabelService' to your module file and
 //  include it in the constructor of your component
 export class ExtendedLabelService extends NovoLabelService {
   dateFormat = 'dd/mm/yyyy';
