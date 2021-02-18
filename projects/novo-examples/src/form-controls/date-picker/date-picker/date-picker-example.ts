e54e6526a04218cffa46c843b13dfa20baf51b50
JorgeBerroa <jorge.berroa@bullhorn.com>
2021-02-18 12:13:12 -0500

    add commment to DatePickerExample component

--- projects/novo-examples/src/form-controls/date-picker/date-picker/date-picker-example.ts
+++ projects/novo-examples/src/form-controls/date-picker/date-picker/date-picker-example.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NovoLabelService } from 'novo-elements';
 
+
+//  Must add 'ExtendedLabelService' to your module file and  
+//  include it in the constructor of your component
 export class ExtendedLabelService extends NovoLabelService {
   dateFormat = 'dd/mm/yyyy';
   dateFormatPlaceholder = 'DD/MM/YYYY';
