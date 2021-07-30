c6cf7bb9f96e44be94d097d87438a7787844e4b7
Michael Dill <mdill@bullhorn.com>
2021-07-30 11:00:54 -0500

    import angular common

--- projects/novo-elements/src/elements/collapsable-column/CollapsableColumn.module.ts
+++ projects/novo-elements/src/elements/collapsable-column/CollapsableColumn.module.ts
@@ -2,12 +2,14 @@
 import { NgModule } from '@angular/core';
 import { NovoCollapsableColumnElement } from './CollapsableColumn.component';
 import { NovoDragulaModule } from '../dragula/Dragula.module';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
     declarations: [NovoCollapsableColumnElement],
     exports: [NovoCollapsableColumnElement],
-    imports: [
-        NovoDragulaModule
-    ]
+  imports: [
+    NovoDragulaModule,
+    CommonModule
+  ]
 })
 export class NovoCollapsableColumnModule {}
