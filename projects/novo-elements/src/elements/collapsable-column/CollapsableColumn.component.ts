4854022f37e54ac0e523428c8dae81ed5332e62f
Michael Dill <mdill@bullhorn.com>
2021-07-30 11:08:42 -0500

    icon support

--- projects/novo-elements/src/elements/collapsable-column/CollapsableColumn.component.ts
+++ projects/novo-elements/src/elements/collapsable-column/CollapsableColumn.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@a
 @Component({
   selector: 'novo-collapsable-column',
   template: `
-    <h1>{{header}}</h1>
+    <h1><i [class]="icon"></i> {{header}}</h1>
     <div [dragula]="dragulaName" [dragulaModel]="dragulaModel" class="card-container">
       <div *ngFor="let card of dragulaModel" class="info-card">
         {{ entity === 'Candidate' ? card?.jobOrder?.title : card?.candidate?.firstName + ' ' + card?.candidate?.lastName }}
@@ -15,6 +15,17 @@ import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@a
 export class NovoCollapsableColumnElement {
   @Input()
   header: string;
+  @Input()
+  set icon(icon: string) {
+    if (icon) {
+      this._icon = `bhi-${icon}`;
+    }
+  }
+  get icon(): string {
+    return this._icon;
+  }
+
+  private _icon: string;
   @Input()
   dragulaName: string;
   @Input()
