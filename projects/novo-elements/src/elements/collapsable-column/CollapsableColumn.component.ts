73342112bce4f1863e4e5ae4890b7a51d6b37096
Michael Dill <mdill@bullhorn.com>
2021-07-30 09:51:14 -0500

    move some stuff from novo to NE

--- projects/novo-elements/src/elements/collapsable-column/CollapsableColumn.component.ts
+++ projects/novo-elements/src/elements/collapsable-column/CollapsableColumn.component.ts
@@ -3,10 +3,22 @@ import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@a
 @Component({
   selector: 'novo-collapsable-column',
   template: `
-    <h1>Collapsable Column</h1>
+    <h1>{{header}}</h1>
+    <div [dragula]="dragulaName" [dragulaModel]="dragulaModel" class="card-container">
+      <div *ngFor="let card of dragulaModel" class="info-card">
+        {{ entity === 'Candidate' ? card?.jobOrder?.title : card?.candidate?.firstName + ' ' + card?.candidate?.lastName }}
+      </div>
+    </div>
   `,
   styleUrls: ['./CollapsableColumn.component.scss']
 })
 export class NovoCollapsableColumnElement {
-
+  @Input()
+  header: string;
+  @Input()
+  dragulaName: string;
+  @Input()
+  dragulaModel: any;
+  @Input()
+  entity: 'Candidate' | 'JobOrder';
 }
--- projects/novo-elements/src/elements/collapsable-column/CollapsableColumn.module.ts
+++ projects/novo-elements/src/elements/collapsable-column/CollapsableColumn.module.ts
@@ -1,9 +1,13 @@
 // NG2
 import { NgModule } from '@angular/core';
 import { NovoCollapsableColumnElement } from './CollapsableColumn.component';
+import {NovoDragulaModule} from 'novo-elements';
 
 @NgModule({
-  declarations: [NovoCollapsableColumnElement],
-  exports: [NovoCollapsableColumnElement],
+    declarations: [NovoCollapsableColumnElement],
+    exports: [NovoCollapsableColumnElement],
+    imports: [
+        NovoDragulaModule
+    ]
 })
 export class NovoCollapsableColumnModule {}
