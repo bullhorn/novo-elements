1f63292c8cd43de4fd47d9c467f9f72d5240c872
Charles Barnes <cbarnes@bullhorn.com>
2022-02-03 19:27:25 -0600

    switch back to right side

--- projects/novo-elements/src/elements/table/Table.ts
+++ projects/novo-elements/src/elements/table/Table.ts
@@ -147,7 +147,7 @@ export enum NovoTableMode {
                   </div>
                   <!-- FILTER DROP-DOWN -->
                   <novo-dropdown
-                    side="top-right"
+                    side="right"
                     *ngIf="config.filtering !== false && column.filtering !== false"
                     class="column-filters"
                     (toggled)="onDropdownToggled($event, column.name)"
