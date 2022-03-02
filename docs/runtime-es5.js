82697cf8870d26cac3208c883221c8b1a89bc949
Brian Kimball <brian.kimball@bullhorn.com>
2022-03-02 16:05:25 -0500

    cleanup for v6.0.0

--- docs/runtime-es5.js
+++ docs/runtime-es5.js
@@ -137,7 +137,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/novo-elements/docs//";
+/******/ 	__webpack_require__.p = "/novo-elements/docs/";
 /******/
 /******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
 /******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
