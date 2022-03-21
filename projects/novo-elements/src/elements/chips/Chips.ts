7d4be1837269f64262d17df5e4c3ea180f93d7b7
Haris Iqbal <hiqbal@bullhorn.com>
2022-03-21 10:59:38 -0400

    fix(Chips): pushed a refactor for checking if the value has changed when setting items

--- projects/novo-elements/src/elements/chips/Chips.ts
+++ projects/novo-elements/src/elements/chips/Chips.ts
@@ -181,9 +181,10 @@ export class NovoChipsElement implements OnInit, ControlValueAccessor {
     }
     this._items.next(this.items);
     const valueToSet = this.source && this.source.valueFormatter ? this.source.valueFormatter(this.items) : this.items.map((i) => i.value);
-    if (Helpers.isBlank(this.value) && !Helpers.isBlank(valueToSet)
-      || !Helpers.isBlank(this.value) && Helpers.isBlank(valueToSet)
-      || !Helpers.isBlank(this.value) && !Helpers.isBlank(valueToSet) && this.value.toString() !== valueToSet.toString()) {
+    if (
+      Helpers.isBlank(this.value) !== Helpers.isBlank(valueToSet) 
+      || JSON.stringify(this.value) !== JSON.stringify(valueToSet) 
+    ) {
       this.value = valueToSet;
       this._propagateChanges();
     }
