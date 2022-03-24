d3585c1b63685fc68bf72060b200422eb339298c
Dan Voegelin <dvoegelin@bullhorn.com>
2022-03-24 13:25:40 -0400

    fixing typing issue caused by https://github.com/microsoft/TypeScript/issues/35865

--- projects/novo-elements/src/elements/agenda/pipe/EndOfWeekDisplayPipe.pipe.ts
+++ projects/novo-elements/src/elements/agenda/pipe/EndOfWeekDisplayPipe.pipe.ts
@@ -4,7 +4,7 @@ import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
 export class EndOfWeekDisplayPipe implements PipeTransform {
   constructor(@Inject(LOCALE_ID) private locale: string = 'en-US') {}
 
-  transform(endOfWeek: Date, startOfWeek: Date, locale: string = this.locale, method: string = 'short'): String {
+  transform(endOfWeek: Date, startOfWeek: Date, locale: string = this.locale, method: 'numeric' | '2-digit' | 'long' | 'short' | 'narrow' = 'short'): String {
     if (endOfWeek.getMonth() === startOfWeek.getMonth()) {
       return new Intl.DateTimeFormat(locale, { day: 'numeric' }).format(endOfWeek);
     }
--- projects/novo-elements/src/elements/agenda/pipe/Month.pipe.ts
+++ projects/novo-elements/src/elements/agenda/pipe/Month.pipe.ts
@@ -3,7 +3,7 @@ import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'month' })
 export class MonthPipe implements PipeTransform {
   constructor(@Inject(LOCALE_ID) private locale: string = 'en-US') {}
-  transform(date: Date, locale: string = this.locale, method: string = 'long'): string {
+  transform(date: Date, locale: string = this.locale, method: 'numeric' | '2-digit' | 'long' | 'short' | 'narrow' = 'long'): string {
     return new Intl.DateTimeFormat(locale, { month: method }).format(date);
   }
 }
