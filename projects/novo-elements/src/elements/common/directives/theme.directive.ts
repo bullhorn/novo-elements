b8c2c94490debf5b1871e01c151594496564fba7
Dan Voegelin <dvoegelin@bullhorn.com>
2022-04-06 11:11:50 -0400

    reverting changes to theme and accent directives

--- projects/novo-elements/src/elements/common/directives/theme.directive.ts
+++ projects/novo-elements/src/elements/common/directives/theme.directive.ts
@@ -1,32 +1,16 @@
 // tslint:disable: directive-selector
-import { ChangeDetectorRef, Directive, ElementRef, HostBinding, Input } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { NovoTheme, ThemeChangeEvent } from '../theme/theme-options';
+import { Directive, ElementRef, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[theme]',
 })
 export class ThemeColorDirective {
-  private subscription: Subscription;
-
   @Input() theme: string;
 
   @HostBinding('class')
   get hb_textColor() {
-    // Support legacy classic theme... for now
-    if (this.novoTheme.themeName === 'classic') {
-      return `novo-theme-${this.theme}`;
-    }
-    return '';
+    return `novo-theme-${this.theme}`;
   }
 
-  constructor(private el: ElementRef, private novoTheme: NovoTheme, protected cdr: ChangeDetectorRef) {
-    this.subscription = this.novoTheme.onThemeChange.subscribe((event: ThemeChangeEvent) => {
-      this.cdr.markForCheck();
-    });
-  }
-
-  onDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+  constructor(private el: ElementRef) {}
 }
