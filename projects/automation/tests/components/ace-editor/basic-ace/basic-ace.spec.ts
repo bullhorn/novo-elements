8e9680a1f8e28f23eeb3a0df4d2924604e4f8863
Kurt Mcgallion <kurt.mcgallion@M45L6QTTWH.local>
2022-05-18 03:58:15 -0600

    Fixed basic-ace test

--- projects/automation/tests/components/ace-editor/basic-ace/basic-ace.spec.ts
+++ projects/automation/tests/components/ace-editor/basic-ace/basic-ace.spec.ts
@@ -1,10 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { PlaywrightDevPage } from '../../../../utils/playwright-dev-page';
 
-test('basic-ace component should match snapshot', async ({ page }) => {
+test.beforeEach(async ({page}) => {
   const playwrightDev = new PlaywrightDevPage(page);
   await playwrightDev.setDateTime('January 01 2022 00:00:00');
   await page.goto('http://localhost:4200/examples?componentName=basic-ace', { waitUntil: 'networkidle' });
+});
+
+test('basic-ace component should match snapshot', async ({ page }) => {
   expect(await page.screenshot({animations: "disabled"})).toMatchSnapshot();
 });
 
