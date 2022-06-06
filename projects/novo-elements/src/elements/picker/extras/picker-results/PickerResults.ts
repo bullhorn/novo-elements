8e53f79f05944ce4f7056bfab1adeffe73396315
kiri-um-bh <79481150+kiri-um-bh@users.noreply.github.com>
2022-06-06 10:46:44 -0500

    style(picker): Added data-automation-id to picker results (#1317)

--- projects/novo-elements/src/elements/picker/extras/picker-results/PickerResults.ts
+++ projects/novo-elements/src/elements/picker/extras/picker-results/PickerResults.ts
@@ -17,6 +17,7 @@ import { BasePickerResults } from '../base-picker-results/BasePickerResults';
         [class.active]="match === activeMatch"
         (mouseenter)="selectActive(match)"
         [class.disabled]="preselected(match)"
+        data-automation-id="picker-result-list-item"
       >
         <item-content> <span [innerHtml]="highlight(match.label, term)"></span> </item-content>
       </novo-list-item>
