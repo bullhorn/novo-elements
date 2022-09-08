009a221aa4ac4c66174b774979643a7f1f775225
kiri.um <kiri.um@bullhorn.com>
2022-09-08 08:17:40 -0500

    feat(data-table): add length

--- projects/novo-elements/src/elements/data-table/data-table.component.ts
+++ projects/novo-elements/src/elements/data-table/data-table.component.ts
@@ -65,7 +65,7 @@ import { DataTableState } from './state/data-table-state.service';
       <novo-data-table-pagination
         *ngIf="paginationOptions"
         [theme]="paginationOptions.theme"
-        [length]="paginationOptions.asyncListCountEnabled? paginationOptions.length : dataSource?.currentTotal"
+        [length]="paginationOptions.asyncListCountEnabled? length : dataSource?.currentTotal"
         [page]="paginationOptions.page"
         [pageSize]="paginationOptions.pageSize"
         [pageSizeOptions]="paginationOptions.pageSizeOptions"
@@ -330,6 +330,7 @@ export class NovoDataTable<T> implements AfterContentInit, OnDestroy {
   @Input() maxSelected: number = undefined;
   @Input() canSelectAll: boolean = false;
   @Input() allMatchingSelected = false;
+  @Input() length: number;
 
   @Input()
   set dataTableService(service: IDataTableService<T>) {
