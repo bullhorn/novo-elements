14282afe0bb75eb2946de877a488496c5d2423d1
Brian Kimball <brian.kimball@bullhorn.com>
2022-09-15 15:36:16 -0400

    fix(build): Removing resize-observer-polyfill

--- projects/elements/common/directives/responsive.directive.ts
+++ projects/elements/common/directives/responsive.directive.ts
@@ -1,6 +1,5 @@
 // tslint:disable: directive-selector
 import { Directive, ElementRef, EventEmitter, Input, OnDestroy, Output, Renderer2 } from '@angular/core';
-import ResizeObserver from 'resize-observer-polyfill';
 
 const entriesMap = new WeakMap();
 
