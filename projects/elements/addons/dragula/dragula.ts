bc3db612cbf4f1fa60128d65dd5395acbaf78338
Dan Voegelin <dvoegelin@bullhorn.com>
2022-11-10 14:08:33 -0500

    refactor(dragula): deprecating dragula directive and service (#1363)

--- projects/elements/addons/dragula/dragula.ts
+++ projects/elements/addons/dragula/dragula.ts
@@ -2,8 +2,17 @@
 import { Directive, ElementRef, Input, OnChanges, OnInit } from '@angular/core';
 // Vendor
 import dragula from '@bullhorn/dragula';
+import { notify } from 'novo-elements/utils';
 import { NovoDragulaService } from './dragula-service';
 
+/**
+* @deprecated since v8.0.0 - slated for deletion.
+*
+* Moving away from all CommonJS dependencies to improve tree-shaking.
+*
+* Please look at built-in ng or third party drag-drop libraries like
+* angular-draggable-droppable, ngx-drag-drop, ngx-sortablejs, ng2-dragula.
+*/
 @Directive({
   selector: '[dragula]',
 })
@@ -16,6 +25,7 @@ export class NovoDragulaElement implements OnInit, OnChanges {
   container: any;
 
   constructor(element: ElementRef, private dragulaService: NovoDragulaService) {
+    notify('[dragula] has been deprecated - please look at built-in ng or third party drag-drop libraries instead');
     this.container = element.nativeElement;
   }
 
