d6cbb16e3823830224da312c5c9b1d7dbf20cc6b
Kurt McGallion <kurt.mcgallion@bullhorn.com>
2023-01-13 12:32:06 -0700

    Updated folder structure

---
+++ projects/automation/tests/layouts/tabs/tabs-router/tabs-router.spec.ts-snapshots/tabs-router.spec.ts-snapshots.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+import { PlaywrightDevPage } from '../../../../../utils/playwright-dev-page';
+
+test('tabs-router.spec.ts-snapshots component should match snapshot', async ({ page }) => {
+  const playwrightDev = new PlaywrightDevPage(page);
+  await playwrightDev.setDateTime('January 01 2022 00:00:00');
+  await page.goto('http://localhost:4200/examples?componentName=tabs-router.spec.ts-snapshots', { waitUntil: 'networkidle' });
+  expect(await page.screenshot({animations: "disabled"})).toMatchSnapshot();
+});
