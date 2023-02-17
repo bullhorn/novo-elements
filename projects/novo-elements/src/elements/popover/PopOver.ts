5602edd145d1bf0b03e4c82735d9cc8f802558f7
wdoironlarue1 <63323624+wdoironlarue1@users.noreply.github.com>
2023-02-17 10:12:19 -0500

    feat(popOver): fix popover error with empty content (#1394)
    * fix console error when clicking on empty popover
    
    * add check for html content
    
    ---------
    
    Co-authored-by: wyatt.doiron-larue <wyatt.doiron-larue@bullhorn.com>

--- projects/novo-elements/src/elements/popover/PopOver.ts
+++ projects/novo-elements/src/elements/popover/PopOver.ts
@@ -98,7 +98,7 @@ export class PopOverDirective implements OnChanges {
   }
 
   show() {
-    if (this.visible) {
+    if (this.visible || (!this.content && !this.popoverHtmlContent)) {
       return;
     }
 
