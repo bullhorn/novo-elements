3e5ad09511bb68037c5de86c2143a5ecc3ed4f6c
Alyssa Bryant <alyssa.bryant@bullhorn.com>
2023-03-01 10:11:55 -0600

    fix(): Revert "feat(Chips): make setItems() async" (#1398)
    Revert "feat(Chips): make setItems() async (#1395)"
    
    This reverts commit 86f37102835c2108b81e0873cadc5a8409dac96c.

--- projects/novo-elements/src/elements/chips/Chips.spec.ts
+++ projects/novo-elements/src/elements/chips/Chips.spec.ts
@@ -1,8 +1,8 @@
 // NG2
 import { async, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-import { NovoLabelService } from '../../services';
-import { ComponentUtils } from '../../utils';
+import { NovoLabelService } from '../../services/novo-label-service';
+import { ComponentUtils } from '../../utils/component-utils/ComponentUtils';
 // App
 import { NovoChipElement } from './Chip';
 import { NovoChipsElement } from './Chips';
@@ -162,9 +162,7 @@ describe('Elements: NovoChipsElement', () => {
         getLabels: (values) => {
           return new Promise((resolve) => {
             values.map((item) => {
-              if (!item.label) {
-                item.label = 'one';
-              }
+              item.label = 'one';
               return item;
             });
             resolve(values);
@@ -176,13 +174,10 @@ describe('Elements: NovoChipsElement', () => {
       expect(component.setItems).toBeDefined();
     });
     it('should retrieve items with labels', (done) => {
-      let spy = jest.spyOn(component, '_propagateChanges');
       component.setItems();
       setTimeout(() => {
         component._items.subscribe((result) => {
           expect(result[0].label).toEqual('one');
-          expect(result[1].label).toEqual('two');
-          expect(spy).toHaveBeenCalled();
           done();
         });
       }, 1);
@@ -216,12 +211,10 @@ describe('Elements: NovoChipsElement', () => {
           ],
         ]),
       };
-      let spy = jest.spyOn(component, '_propagateChanges');
       component.setItems();
       setTimeout(() => {
         component._items.subscribe((result) => {
           expect(result).toEqual(component.model);
-          expect(spy).toHaveBeenCalled();
           done();
         });
       }, 1);
