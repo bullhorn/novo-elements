6d04ff04ce71f30afc3de2c83c719abf5bf9700e
Dan Voegelin <dvoegelin@bullhorn.com>
2023-03-08 15:03:18 -0500

    chore(config): changing formatting of new releaseRules

--- release.config.js
+++ release.config.js
@@ -12,12 +12,14 @@ module.exports = {
     },
   ],
   plugins: [
-    '@semantic-release/commit-analyzer', {
-      preset: 'angular',
-      releaseRules: [
-        { type: 'chore', release: 'patch' },
-      ],
-    },
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          { type: 'chore', release: 'patch' },
+        ],
+      },
+    ]
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     [
