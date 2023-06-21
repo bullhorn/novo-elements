6f781d435898fd5c0faa06b239691805126a8288
Dan Voegelin <dvoegelin@bullhorn.com>
2023-06-21 16:38:38 -0400

    reverting header scss filename

--- projects/novo-elements/src/elements/header/Header.ts
+++ projects/novo-elements/src/elements/header/Header.ts
@@ -4,7 +4,7 @@ import { BooleanInput } from 'novo-elements/utils';
 @Component({
   selector: 'header-spacer',
   template: `<ng-content></ng-content>`,
-  styleUrls: ['./header-spacer.scss'],
+  styleUrls: ['./HeaderSpacer.scss'],
 })
 export class NovoHeaderSpacer {}
 
@@ -66,7 +66,7 @@ export class NovoUtilActionComponent {
     </section>
     <ng-content></ng-content>
   `,
-  styleUrls: ['./header.scss'],
+  styleUrls: ['./Header.scss'],
 })
 export class NovoHeaderComponent {
   @HostBinding('attr.role')
