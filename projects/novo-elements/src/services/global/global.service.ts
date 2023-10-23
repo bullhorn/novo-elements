306ebb5b7eee037e5aa8d7e10af42e06878e8b72
David Keyworth <130384363+davidkbh@users.noreply.github.com>
2023-10-23 09:12:38 -0400

    refactor(FileInput): Removed Dragula from FileInput.ts (#1470)

--- projects/novo-elements/src/services/global/global.service.ts
+++ projects/novo-elements/src/services/global/global.service.ts
@@ -4,6 +4,7 @@ export interface Global {}
 
 export abstract class GlobalRef {
   abstract get nativeGlobal(): Global;
+  abstract get nativeWindow(): Window;
 }
 
 @Injectable()
@@ -11,10 +12,16 @@ export class BrowserGlobalRef extends GlobalRef {
   get nativeGlobal(): Global {
     return window as Global;
   }
+  get nativeWindow(): Window {
+    return window;
+  }
 }
 export class NodeGlobalRef extends GlobalRef {
   get nativeGlobal(): Global {
     throw new Error(`global doesn't compile for some reason`);
     // return global as Global;
+  }
+  get nativeWindow(): Window {
+    throw new Error('Node does not have a window object');
   }
 }
