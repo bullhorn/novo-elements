db0da47879dd85e509bf52e18eeae88325fa3cf0
David Keyworth <david.keyworth@bullhorn.com>
2024-10-18 09:51:57 -0400

    Renamed variable

--- projects/novo-elements/src/elements/chips/ChipInput.ts
+++ projects/novo-elements/src/elements/chips/ChipInput.ts
@@ -127,10 +127,10 @@ export class NovoChipInput implements NovoChipTextControl, OnChanges, OnDestroy 
   }
 
   /** Checks to see if the blur should emit the (chipEnd) event. */
-  _blur(blurEvt: FocusEvent) {
+  _blur(blurEvent: FocusEvent) {
     if (this.addOnBlur) {
       this._emitChipEnd();
-    } else if (!this._field.blurEventIsInField(blurEvt)) {
+    } else if (!this._field.blurEventIsInField(blurEvent)) {
       this.clearValue();
     }
     this.focused = false;
