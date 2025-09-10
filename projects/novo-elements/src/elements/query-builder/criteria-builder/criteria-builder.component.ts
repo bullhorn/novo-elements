b1f82444c76f8f4cc5addfebafa24ea39c7ef299
Peter Monroe <peter.monroe@bullhorn.com>
2025-09-10 11:23:20 -0500

    feat(QueryBuilder): Add the ability to use entity as a class on condition group

--- projects/novo-elements/src/elements/query-builder/criteria-builder/criteria-builder.component.ts
+++ projects/novo-elements/src/elements/query-builder/criteria-builder/criteria-builder.component.ts
@@ -207,7 +207,7 @@ export class CriteriaBuilderComponent implements OnInit, OnDestroy, AfterContent
     return this.formBuilder.group(controls);
   }
 
-  newCondition({ field, operator, scope, value, supportingValue }: Condition = EMPTY_CONDITION): UntypedFormGroup {
+  newCondition({ field, operator, scope, value, supportingValue, entity }: Condition = EMPTY_CONDITION): UntypedFormGroup {
     return this.formBuilder.group({
       conditionType: '$and',
       field: [field, Validators.required],
@@ -215,6 +215,7 @@ export class CriteriaBuilderComponent implements OnInit, OnDestroy, AfterContent
       scope: [scope],
       value: [value],
       supportingValue: [supportingValue],
+      entity: [entity],
     });
   }
 
