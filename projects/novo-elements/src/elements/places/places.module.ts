2ce4f1d06b583ff845843c33be43588aa446386a
Nathan Dickerson <nathan.dickerson@bullhorn.com>
2025-09-24 10:35:26 -0500

    breaking(upgrade): Angular 18 build migration

--- projects/novo-elements/src/elements/places/places.module.ts
+++ projects/novo-elements/src/elements/places/places.module.ts
@@ -1,15 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NovoListModule } from 'novo-elements/elements/list';
 import { PlacesListComponent } from './places.component';
 import { GooglePlacesService } from './places.service';
 
-@NgModule({
-  declarations: [PlacesListComponent],
-  imports: [CommonModule, HttpClientModule, FormsModule, NovoListModule],
-  exports: [PlacesListComponent],
-  providers: [GooglePlacesService],
-})
+@NgModule({ declarations: [PlacesListComponent],
+    exports: [PlacesListComponent], imports: [CommonModule, FormsModule, NovoListModule], providers: [GooglePlacesService, provideHttpClient(withInterceptorsFromDi())] })
 export class GooglePlacesModule {}
