029a628c452f4372754f3606d02f2fd470f29441
David Keyworth <130384363+davidkbh@users.noreply.github.com>
2025-10-31 11:44:52 -0400

    feat(DragDropModule): Updates to drag/drop, and implementation in Data Table (#1720)
    feat(DragDropModule): Updates to dragging and implementation in Novo Data Table
        Added classes to represent state
        Fixed a case where an object would stutter back and forth
        Fixed dragging onto tooltips/overlays
        Added a function property to mark some drag items as disabled
        Add an 'eventSrc' property to the NovoDataTable preferences changed event, to indicate changes from dragging/other actions
        Add auto-scroll when dragging, even on top of draggable items.

--- projects/novo-elements/src/elements/drag-drop/drag-drop-box.ts
+++ projects/novo-elements/src/elements/drag-drop/drag-drop-box.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Directive, ElementRef, EventEmitter, HostListener, Input, OnDestroy, Output, Renderer2 } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { AfterViewInit, computed, Directive, ElementRef, EventEmitter, HostBinding, HostListener, inject, Input, OnDestroy, Output, Renderer2, signal } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 
 export interface NovoDragFinishEvent<T> {
@@ -10,27 +11,54 @@ export interface NovoDragFinishEvent<T> {
 interface NovoDragItem<T> {
     item: T;
     element: HTMLElement;
+    removeListeners: () => void;
 }
+
+const SCROLL_DRAGGABLE_PADDING_REGION = 30;
+const SCROLL_DRAGGABLE_EVENT_AMT = 10;
 
 @Directive({
     selector: '[novoDragDrop]',
-    standalone: false
+    standalone: false,
+    host: {
+        class: 'novo-drag-container',
+        '[class.dragging]': '!!pickedUp',
+    }
 })
 export class NovoDragBoxParent<T> implements AfterViewInit, OnDestroy {
 
     pickedUp?: HTMLElement;
+    previewReplacing?: HTMLElement;
     savedOrder?: NovoDragItem<T>[];
 
-    $destroy = new ReplaySubject<void>(1);
+    overlayContainer = inject(OverlayContainer, { optional: true });
 
     @Input('novoDragDrop') items: T[];
+    /**
+     * A function to specify whether a given item in the child list is draggable.
+     * (Note that altering this after setup is not yet supported)
+     */
+    @Input('novoDragDropFilter') dragFilter?: (item: T) => boolean;
+    /**
+     * Prevents behavior that eases the act of dragging an object to the edge of a scrollable container. 
+     */
+    @Input('novoDragDropDisableScroll') disableScroll?: boolean;
 
     @Output() novoDragDropFinish = new EventEmitter<NovoDragFinishEvent<T>>();
 
     private trackedItems: NovoDragItem<T>[];
+    private scrollContainer = signal<HTMLElement>(null);
+    private scrollContainerRect = computed(() => this.scrollContainer()?.getBoundingClientRect());
+    private scrollX = signal<boolean>(false);
+    private scrollY = signal<boolean>(false);
 
     get itemsReordered(): T[] {
         return this.trackedItems.map(item => item.item);
+    }
+
+    @HostBinding('class.dragging')
+    get dragging() {
+        return Boolean(this.pickedUp);
     }
 
     mutationObserver = new MutationObserver(this.mutationDetected.bind(this));
@@ -42,38 +70,50 @@ export class NovoDragBoxParent<T> implements AfterViewInit, OnDestroy {
     }
 
     ngAfterViewInit(): void {
-        this.registerChildren();
-        this.mutationObserver.observe(this.element, { childList: true });
+        if (this.items) {
+            this.registerChildren();
+            this.mutationObserver.observe(this.element, { childList: true });
+        }
+        // if items is falsey, then no behavior is set up. In future, this component may support late enablement,
+        // but for now it's not a priority. (It would open the question of late disablement)
     }
 
     ngOnDestroy(): void {
-        this.$destroy.next();
-        this.$destroy.complete();
+        this.trackedItems?.forEach(item => {
+            item.removeListeners();
+        });
     }
 
     private registerChildren(): void {
-        if (this.items && this.items.length !== this.element.children.length) {
-            throw new Error(`Could not match item list to children list - drag box contains ${this.items.length} items, but has ${this.element.children.length} elements`);
+        if (this.items) {
+            if (this.items.length !== this.element.children.length) {
+                throw new Error(`Could not match item list to children list - drag box contains ${this.items.length} items, but has ${this.element.children.length} elements`);
+            }
+            this.trackedItems = [];
+            for (let i = 0; i < this.element.children.length; i++) {
+                this.registerChild(this.element.children[i] as HTMLElement, i);
+            }
         }
-        this.trackedItems = [];
-        for (let i = 0; i < this.element.children.length; i++) {
-            this.registerChild(this.element.children[i] as HTMLElement, i);
-        }
-
     }
 
     private registerChild(element: HTMLElement, index: number) {
-        const listeners = [
-            this.renderer.listen(element, 'dragstart', this.onDragStart.bind(this)),
-            this.renderer.listen(element, 'drop', this.onDragFinish.bind(this)),
-            this.renderer.listen(element, 'dragend', this.onDragStop.bind(this))
-        ];
-        element.draggable = true;
-        this.$destroy.subscribe(() => listeners.forEach(cb => cb()));
-        this.trackedItems.push({
+        const trackedItem: Partial<NovoDragItem<T>> = {
             item: this.items[index],
             element
-        });
+        };
+        if (this.dragFilter && !this.dragFilter(this.items[index])) {
+            element.draggable = false;
+            trackedItem.removeListeners = () => {};
+        } else {
+            const listeners = [
+                this.renderer.listen(element, 'dragstart', this.onDragStart.bind(this)),
+                this.renderer.listen(element, 'drop', this.onDragFinish.bind(this)),
+                this.renderer.listen(element, 'dragend', this.onDragStop.bind(this))
+            ];
+            element.draggable = true;
+            trackedItem.removeListeners = () => listeners.forEach(cb => cb());
+        }
+        this.trackedItems.push(trackedItem as NovoDragItem<T>);
     }
 
     mutationDetected(mutations: MutationRecord[]) {
@@ -100,7 +140,13 @@ export class NovoDragBoxParent<T> implements AfterViewInit, OnDestroy {
         });
         if (removedNodes.size > 0) {
             this.trackedItems = this.trackedItems.filter(
-                item => !removedNodes.has(item.element));
+                item => {
+                    const keep = !removedNodes.has(item.element);
+                    if (!keep) {
+                        item.removeListeners();
+                    }
+                    return keep;
+                });
         }
     }
 
@@ -114,6 +160,10 @@ export class NovoDragBoxParent<T> implements AfterViewInit, OnDestroy {
         const dataTransfer = event.dataTransfer;
         // Present a native 'move item' effect
         dataTransfer.effectAllowed = 'move';
+        if (!this.disableScroll) {
+            this.scrollContainer.set(this.findScrollableParent());
+        }
+
         this.pickedUp = event.target as HTMLElement;
         event.stopPropagation();
         this.savedOrder = [...this.trackedItems];
@@ -122,6 +172,7 @@ export class NovoDragBoxParent<T> implements AfterViewInit, OnDestroy {
     // Equivalent of "finally" - this runs whether or not the drag finished on a valid ending location
     onDragStop(event: DragEvent): void {
         this.pickedUp = null;
+        this.previewReplacing = null;
         this.savedOrder = null;
         event.stopPropagation();
     }
@@ -153,7 +204,7 @@ export class NovoDragBoxParent<T> implements AfterViewInit, OnDestroy {
 
     @HostListener('window:dragover', ['$event'])
     onDragOver(event: DragEvent): void {
-        if (!this.pickedUp) {
+        if (!this.pickedUp || this.inOverlay(event)) {
             return;
         }
         let target = event.target as HTMLElement;
@@ -172,7 +223,14 @@ export class NovoDragBoxParent<T> implements AfterViewInit, OnDestroy {
             if (event.dataTransfer) {
                 event.dataTransfer.dropEffect = 'move';
             }
-            this.applyTempSort(this.pickedUp, target);
+            if (this.previewReplacing !== target) {
+                if (this.isValidTarget(target)) {
+                    this.previewReplacing = target;
+                    this.applyTempSort(this.pickedUp, target);
+                } else {
+                    event.dataTransfer.dropEffect = 'none';
+                }
+            }
         } else {
             // if not within this drag box, then move this item back to its original position and show a diabled drag effect
             if (event.dataTransfer) {
@@ -180,6 +238,20 @@ export class NovoDragBoxParent<T> implements AfterViewInit, OnDestroy {
             }
             this.resetSorting();
         }
+        if (!this.disableScroll) {
+            this.processScrollDuringMove(event);
+        }
+    }
+
+    private inOverlay(event: DragEvent): boolean {
+        // Make sure a drag event is not looking at a tooltip or similar.
+        // Return false if the dragbox itself is in an overlay - these scenarios will be harder to account for.
+        const overlayElement = this.overlayContainer?.getContainerElement();
+        return overlayElement && (overlayElement.contains(event.target as Node) && !overlayElement.contains(this.element));
+    }
+
+    private isValidTarget(target: HTMLElement): boolean {
+        return target.draggable;
     }
 
     private findDraggableParentOfElement(target: HTMLElement): HTMLElement | null {
@@ -238,10 +310,70 @@ export class NovoDragBoxParent<T> implements AfterViewInit, OnDestroy {
         }
     }
 
+    private processScrollDuringMove(event: DragEvent) {
+        const scrollElement = this.scrollContainer();
+        if (!scrollElement) {
+            return;
+        }
+        const rect = this.scrollContainerRect();
+        if (this.scrollX()) {
+            if (event.x > rect.left && event.x < rect.left + SCROLL_DRAGGABLE_PADDING_REGION) {
+                scrollElement.scrollLeft -= 10;
+            } else if (event.x < rect.right && event.x > rect.right - SCROLL_DRAGGABLE_PADDING_REGION) {
+                scrollElement.scrollLeft += 10;
+            }
+        }
+        if (this.scrollY()) {
+            if (event.y > rect.top && event.y < rect.top + SCROLL_DRAGGABLE_PADDING_REGION) {
+                scrollElement.scrollTop -= 10;
+            } else if (event.y < rect.bottom && event.y > rect.bottom - SCROLL_DRAGGABLE_PADDING_REGION) {
+                scrollElement.scrollTop += 10;
+            }
+        }
+    }
+
     public isElementWithinEventBounds(element: Element, event: DragEvent) {
         const rect = element.getBoundingClientRect();
         const isInside = event.clientX > rect.left && event.clientX < rect.right &&
             event.clientY < rect.bottom && event.clientY > rect.top;
         return isInside;
     }
+
+    private findScrollableParent(): HTMLElement | null {
+        let currentElement = this.element.parentElement;
+        this.scrollX.set(false);
+        this.scrollY.set(false);
+        
+        while (currentElement) {
+            const hasVerticalScroll = currentElement.scrollHeight > currentElement.clientHeight;
+            const hasHorizontalScroll = currentElement.scrollWidth > currentElement.clientWidth;
+            const isScrollable = hasVerticalScroll || hasHorizontalScroll;
+            
+            if (isScrollable) {
+                const computedStyle = window.getComputedStyle(currentElement);
+                const overflowY = computedStyle.overflowY;
+                const overflowX = computedStyle.overflowX;
+                
+                // Check if overflow is set to allow scrolling
+                if ((overflowY === 'auto' || overflowY === 'scroll') || 
+                    (overflowX === 'auto' || overflowX === 'scroll')) {
+                    // Verify the scrollable parent is smaller than the drag container
+                    if (currentElement.clientHeight < this.element.clientHeight) {
+                        this.scrollY.set(true);
+                    }
+                    if (currentElement.clientWidth < this.element.clientWidth) {
+                        this.scrollX.set(true);
+                    }
+                    if (this.scrollX() || this.scrollY()) {
+                        return currentElement;
+                    }
+                }
+            }
+            
+            currentElement = currentElement.parentElement;
+        }
+        
+        return null;
+    }
+
 }
