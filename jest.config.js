d4eb3442c2961cc9bed8d70ea9716462878a86db
Nathan Dickerson <nathan.dickerson@bullhorn.com>
2026-01-29 12:09:28 -0600

    chore(upgrade): fixing unit test imports

--- jest.config.js
+++ jest.config.js
@@ -12,9 +12,12 @@ module.exports = {
       {
         tsconfig: '<rootDir>/tsconfig.spec.json',
         stringifyContentPathRegex: '\\.html$',
+        isolatedModules: true,
       }
     ]
   },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
   moduleNameMapper: {
     '^novo-elements$': '<rootDir>/projects/novo-elements/src/index',
     '^novo-elements/(.*)$': '<rootDir>/projects/novo-elements/src/$1',
