4da954d1c5b01ae4a05b89e25a40afccadf0fdc7
Nathan Dickerson <nathan.dickerson@bullhorn.com>
2026-02-27 09:41:22 -0600

    chore(upgrade): fix import

--- projects/novo-elements/src/elements/calendar/strategies/week-selection.strategy.ts
+++ projects/novo-elements/src/elements/calendar/strategies/week-selection.strategy.ts
@@ -1,6 +1,5 @@
 import { Day } from 'date-fns';
-import type { DateLike, NovoDateSelectionStrategy } from 'novo-elements/utils';
-import { DateUtil } from 'novo-elements/utils';
+import { DateUtil, type DateLike, type NovoDateSelectionStrategy } from 'novo-elements/utils';
 
 export class WeekSelectionStrategy implements NovoDateSelectionStrategy<DateLike[]> {
   constructor(private weekStartsOn: Day = 0) {}
