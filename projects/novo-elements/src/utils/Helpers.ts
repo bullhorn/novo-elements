74b1a1ecca89bec7aea889bcced2dd7d5e858af3
David Keyworth <david.keyworth@bullhorn.com>
2026-05-20 09:06:35 -0500

    Add helper function focusWalker to find next focusable element

--- projects/novo-elements/src/utils/Helpers.ts
+++ projects/novo-elements/src/utils/Helpers.ts
@@ -550,3 +550,30 @@ export function binarySearch<T>(item: T, array: T[], compare: (a: T, b: T) => 1 
     }
   }
 }
+
+export interface NextFocusOpts {
+  allowExit: boolean;
+}
+
+const NEXT_FOCUS_OPTS_DEFAULT = {
+  allowExit: false
+};
+
+const FOCUSABLE_ELEMENTS = ['BUTTON', 'INPUT', 'A', 'SELECT', 'TEXTAREA'];
+
+export function focusWalker(startingPosition: HTMLElement, opts?: Partial<NextFocusOpts>) {
+  opts = { ...NEXT_FOCUS_OPTS_DEFAULT, ...opts };
+  return startingPosition.ownerDocument.createTreeWalker(startingPosition, NodeFilter.SHOW_ELEMENT, {
+    acceptNode(node: HTMLElement) {
+      if (!opts.allowExit && !startingPosition.contains(node)) {
+        return NodeFilter.FILTER_REJECT;
+      }
+      if (node.tabIndex === 0 || FOCUSABLE_ELEMENTS.includes(node.tagName)) {
+        return NodeFilter.FILTER_ACCEPT;
+      } else {
+        return NodeFilter.FILTER_SKIP;
+      }
+    }
+  });
+}
+const x = focusWalker(document.body);
